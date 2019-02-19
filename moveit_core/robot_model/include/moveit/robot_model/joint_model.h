9b507b1267056a0ba33e1f30194b7b2925ed77bf
Anasarrak <anasarrak@gmail.com>
2019-02-19 17:03:41 +0100

    Remove unnecesary log declarations

--- moveit_core/robot_model/include/moveit/robot_model/joint_model.h
+++ moveit_core/robot_model/include/moveit/robot_model/joint_model.h
@@ -47,9 +47,10 @@
 #include <random_numbers/random_numbers.h>
 #include <Eigen/Geometry>
 #include <rcutils/logging_macros.h>
+#include <moveit/logging/logging.h>
 
-#define ROS_DEBUG_NAMED RCUTILS_LOG_ERROR
-#define ROS_WARN_NAMED RCUTILS_LOG_ERROR
+// #define ROS_DEBUG_NAMED RCUTILS_LOG_ERROR
+// #define ROS_WARN_NAMED RCUTILS_LOG_ERROR
 
 namespace moveit
 {
