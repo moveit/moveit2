7c4d254f71c0794726abb75e9254a2170a5a02fc
LanderU <lander.usategui@gmail.com>
2019-02-27 11:52:53 +0100

    Added ros2 download

--- osx-compile.sh
+++ osx-compile.sh
@@ -3,6 +3,12 @@
 set -x #Debug
 set -e #exit on failure
 
+#Prepare for ros2
+cd /tmp
+wget https://github.com/ros2/ros2/releases/download/release-crystal-20190214/ros2-crystal-20190214-macos-amd64.tar.bz2
+tar jxf ros2-crystal-20190214-macos-amd64.tar.bz2
+
+source ros2-osx/setup.bash
 mkdir -p /tmp/ros2_ws/src
 cp -r /tmp/moveit2 /tmp/ros2_ws/src
 cd /tmp/ros2_ws && wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
