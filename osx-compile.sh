88bf53c052342cf5270207ac5ee2f9746be30f42
LanderU <lander.usategui@gmail.com>
2019-02-27 11:15:20 +0100

    Added failure flag

--- osx-compile.sh
+++ osx-compile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -x #Debug
+set -e #exit on failure
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   mkdir -p /tmp/ros2_ws/src
   cp -r /tmp/moveit2 /tmp/ros2_ws/src
-  cd /tmp/ros2 && wget wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
+  cd /tmp/ros2_ws && wget wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
   vcs import src < external-repos.repos
   colcon build --merge-install
 fi
