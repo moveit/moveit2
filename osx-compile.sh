15af44c83fc2547f7cf61c5ded7ad2d845e037d1
LanderU <lander.usategui@gmail.com>
2019-02-27 12:54:08 +0100

    Added OpenSSL to /home/lander/bin:/home/lander/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin

--- osx-compile.sh
+++ osx-compile.sh
@@ -13,4 +13,5 @@ mkdir -p /tmp/ros2_ws/src
 cp -r /tmp/moveit2 /tmp/ros2_ws/src
 cd /tmp/ros2_ws && wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
 vcs import src < external-repos.repos
+export PATH="/usr/local/opt/openssl/bin:$PATH"
 colcon build --merge-install
