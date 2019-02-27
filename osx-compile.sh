63c9bdfe11d1e85b7987dcd79ba2cbc5ea7b229d
LanderU <lander.usategui@gmail.com>
2019-02-27 11:25:16 +0100

    Fix wget command

--- osx-compile.sh
+++ osx-compile.sh
@@ -6,7 +6,7 @@ set -e #exit on failure
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   mkdir -p /tmp/ros2_ws/src
   cp -r /tmp/moveit2 /tmp/ros2_ws/src
-  cd /tmp/ros2_ws && wget wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
+  cd /tmp/ros2_ws && wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
   vcs import src < external-repos.repos
   colcon build --merge-install
 fi
