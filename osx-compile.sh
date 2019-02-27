29b6121844342a4ce51f94b4e080a4252f3189a8
LanderU <lander.usategui@gmail.com>
2019-02-27 11:35:15 +0100

    Test, remove if on bash

--- osx-compile.sh
+++ osx-compile.sh
@@ -3,10 +3,8 @@
 set -x #Debug
 set -e #exit on failure
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  mkdir -p /tmp/ros2_ws/src
-  cp -r /tmp/moveit2 /tmp/ros2_ws/src
-  cd /tmp/ros2_ws && wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
-  vcs import src < external-repos.repos
-  colcon build --merge-install
-fi
+mkdir -p /tmp/ros2_ws/src
+cp -r /tmp/moveit2 /tmp/ros2_ws/src
+cd /tmp/ros2_ws && wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
+vcs import src < external-repos.repos
+colcon build --merge-install
--- osx-dependencies.sh
+++ osx-dependencies.sh
@@ -2,12 +2,10 @@
 
 #FROM https://index.ros.org/doc/ros2/Installation/OSX-Development-Setup/
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  brew doctor
-  brew install cppcheck pcre poco tinyxml
-  brew install asio tinyxml2
-  brew install log4cxx
-  python3 -m pip install argcomplete catkin_pkg colcon-common-extensions coverage empy flake8 flake8-blind-except \
-  flake8-builtins flake8-class-newline flake8-comprehensions flake8-deprecated flake8-docstrings flake8-import-order flake8-quotes \
-  git+https://github.com/lark-parser/lark.git@0.7d mock nose pep8 pydocstyle pyparsing setuptools vcstool
-fi
+brew doctor
+brew install cppcheck pcre poco tinyxml
+brew install asio tinyxml2
+brew install log4cxx
+python3 -m pip install argcomplete catkin_pkg colcon-common-extensions coverage empy flake8 flake8-blind-except \
+flake8-builtins flake8-class-newline flake8-comprehensions flake8-deprecated flake8-docstrings flake8-import-order flake8-quotes \
+git+https://github.com/lark-parser/lark.git@0.7d mock nose pep8 pydocstyle pyparsing setuptools vcstool
