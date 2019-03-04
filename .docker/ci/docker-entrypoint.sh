724b5284760153a849606108b29d8d145bad939d
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-03-04 18:07:49 +0100

    Update docker-entrypoint.sh

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-/bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen -exec rm -rf {} \; 2>/dev/null && source /opt/ros/crystal/setup.bash && colcon build --merge-install"
+/bin/bash -c "find /opt/ros/crystal/ -name tf2_eiegen | xargs rm -rf && source /opt/ros/crystal/setup.bash && colcon build --merge-install"
