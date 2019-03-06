679d158a2cf4c870c5dafd7f572b6dc54073181c
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-03-06 10:51:58 +0100

    Prepare for perception
    * freeglut 
    * glew

--- osx-dependencies.sh
+++ osx-dependencies.sh
@@ -3,13 +3,7 @@
 #FROM https://index.ros.org/doc/ros2/Installation/OSX-Development-Setup/
 
 brew doctor
-brew install cppcheck pcre poco tinyxml openssl
-brew install asio tinyxml2
-brew install log4cxx
-brew install assimp
-brew install qhull
-brew install boost
-brew install fcl
+brew install cppcheck pcre poco tinyxml openssl asio tinyxml2 log4cxx assimp qhull boost fcl freeglut glew
 python3 -m pip install argcomplete catkin_pkg colcon-common-extensions coverage empy flake8 flake8-blind-except \
 flake8-builtins flake8-class-newline flake8-comprehensions flake8-deprecated flake8-docstrings flake8-import-order flake8-quotes \
 git+https://github.com/lark-parser/lark.git@0.7d mock nose pep8 pydocstyle pyparsing setuptools vcstool
