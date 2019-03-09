ec3915039399daa725a861ffc588ddec1ccfe3eb
LanderU <lander.usategui@gmail.com>
2019-03-09 12:45:14 +0100

    Fix image_common

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-/bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash && colcon build --merge-install"
+/bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash \
+              && touch src/image_common/camera_calibration_parsers/COLCON_IGNORE \
+              && touch src/image_common/camera_info_manager/COLCON_IGNORE \
+              && colcon build --merge-install"
