d0e257f7d1a69691bbed0b2d162c61caca935f69
LanderU <lander.usategui@gmail.com>
2019-03-09 13:03:20 +0100

    docker-entrypoint.sh: use packages-skip

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -3,6 +3,4 @@
 set -e
 
 /bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash \
-              && touch /ros2_ws/src/image_common/camera_calibration_parsers/COLCON_IGNORE \
-              && touch /ros2_ws/src/image_common/camera_info_manager/COLCON_IGNORE \
-              && colcon build --merge-install"
+              && colcon build --merge-install --packages-skip camera_calibration_parsers camera_info_manager"
