e905921a1b69ac6bc2c7e8814952dd92a9a3eab3
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-03-09 17:27:08 +0100

    Update docker-entrypoint.sh
    Added COLCON_IGNORE

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-/bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash && colcon build --merge-install"
+/bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash \
+	      && touch /ros2_ws/src/image_common/camera_calibration_parsers/COLCON_IGNORE \
+              && touch /ros2_ws/src/image_common/camera_info_manager/COLCON_IGNORE \
+              && colcon build --merge-install"
