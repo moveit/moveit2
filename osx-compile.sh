fa82fc58dadf452e4c06529eae18704715377bbc
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-03-10 17:35:06 +0100

    Update CI to ROS2
    Authored by @LanderU

---
+++ osx-compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#set -x #Debug
+set -e #exit on failure
+
+#Prepare for ros2
+cd /tmp
+wget https://github.com/ros2/ros2/releases/download/release-crystal-20190214/ros2-crystal-20190214-macos-amd64.tar.bz2
+tar jxf ros2-crystal-20190214-macos-amd64.tar.bz2
+# Remove tf2_eigen
+find ros2-osx/ -name tf2_eigen | xargs rm -rf
+
+source ros2-osx/setup.bash
+mkdir -p /tmp/ros2_ws/src
+cp -r /tmp/moveit2 /tmp/ros2_ws/src
+cd /tmp/ros2_ws && wget https://raw.githubusercontent.com/AcutronicRobotics/moveit2/master/external-repos.repos
+vcs import src < external-repos.repos
+export OPENSSL_ROOT_DIR="/usr/local/opt/openssl"
+#Ignore packages
+touch src/image_common/camera_calibration_parsers/COLCON_IGNORE
+touch src/image_common/camera_info_manager/COLCON_IGNORE
+colcon build --merge-install
