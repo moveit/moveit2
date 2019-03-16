8b4f35c57334d4d18025919e336b62e0daf80b45
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-03-16 19:54:07 +0100

    Meet the LOGGER indications
    Follows from https://github.com/ros-planning/moveit2/pull/8#discussion_r265828690
    taking into account that recommendations do currently fail
    to compile given the structure of RCLCPP macros

--- moveit_core/kinematics_base/include/moveit/kinematics_base/kinematics_base.h
+++ moveit_core/kinematics_base/include/moveit/kinematics_base/kinematics_base.h
@@ -59,7 +59,7 @@ MOVEIT_CLASS_FORWARD(RobotModel)
 namespace kinematics
 {
 // Logger
-rclcpp::Logger logger_kinematics_base = rclcpp::get_logger("kinematics_base");
+rclcpp::Logger LOGGER = rclcpp::get_logger("kinematics_base");
 
 /*
  * @enum DiscretizationMethods
@@ -314,7 +314,7 @@ public:
     }
 
     // Otherwise throw error because this function should have been implemented
-    RCLCPP_ERROR(kinematics::logger_kinematics_base, "This kinematic solver does not support searchPositionIK with multiple poses");
+    RCLCPP_ERROR(kinematics::LOGGER, "This kinematic solver does not support searchPositionIK with multiple poses");
     return false;
   }
 
@@ -438,7 +438,7 @@ public:
   virtual const std::string& getTipFrame() const
   {
     if (tip_frames_.size() > 1)
-      RCLCPP_ERROR(kinematics::logger_kinematics_base, "This kinematic solver has more than one tip frame, "
+      RCLCPP_ERROR(kinematics::LOGGER, "This kinematic solver has more than one tip frame, "
                                          "do not call getTipFrame()");
 
     return tip_frames_[0];
