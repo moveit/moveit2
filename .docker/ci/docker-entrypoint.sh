595325b96659bccbfdb905c5fbca815c7dee81eb
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-03-17 09:57:24 +0100

    Update docker-entrypoint.sh

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 /bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash \
-	            && touch /ros2_ws/src/image_common/camera_calibration_parsers/COLCON_IGNORE \
-              && touch /ros2_ws/src/image_common/camera_info_manager/COLCON_IGNORE \
+	       && touch /root/ws_moveit/src/image_common/camera_calibration_parsers/COLCON_IGNORE \
+              && touch /root/ws_moveit/src/image_common/camera_info_manager/COLCON_IGNORE \
               && colcon build --merge-install"
