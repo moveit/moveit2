179d09404944bec8ac4fd8b166b7281e48da464f
anasarrak <anasarrak@gmail.com>
2019-03-26 14:16:47 +0100

    Update controller_manager.h

--- moveit_core/controller_manager/include/moveit/controller_manager/controller_manager.h
+++ moveit_core/controller_manager/include/moveit/controller_manager/controller_manager.h
@@ -138,7 +138,7 @@ public:
    * Return true if the execution is complete (whether successful or not).
    * Return false if timeout was reached.
    * If timeout is 0 (default argument), wait until the execution is complete (no timeout). */
-  virtual bool waitForExecution(const ros::Duration& timeout = ros::Duration(0)) = 0;
+  virtual bool waitForExecution(const rclcpp::Duration& timeout = rclcpp::Duration(0)) = 0;
 
   /** \brief Return the execution status of the last trajectory sent to the controller. */
   virtual ExecutionStatus getLastExecutionStatus() = 0;
