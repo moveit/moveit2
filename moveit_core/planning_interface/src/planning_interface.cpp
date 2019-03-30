3d03f3d5e61056895b26fa3e9359b5da008a1368
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-03-30 23:40:37 +0100

    planning_interface, adapt logger to https://github.com/ros-planning/moveit2/issues/21

--- moveit_core/planning_interface/src/planning_interface.cpp
+++ moveit_core/planning_interface/src/planning_interface.cpp
@@ -40,7 +40,7 @@
 
 namespace planning_interface
 {
-    rclcpp::Logger logger = rclcpp::get_logger("planning_interface");
+rclcpp::Logger LOGGER = rclcpp::get_logger("moveit").get_child("planning_interface");
 
 namespace
 {
@@ -82,13 +82,13 @@ void PlanningContext::setMotionPlanRequest(const MotionPlanRequest& request)
   request_ = request;
   if (request_.allowed_planning_time <= 0.0)
   {
-    RCLCPP_INFO(logger,
+    RCLCPP_INFO(LOGGER,
                    "The timeout for planning must be positive (%lf specified). Assuming one second instead.",
                    request_.allowed_planning_time);
     request_.allowed_planning_time = 1.0;
   }
   if (request_.num_planning_attempts < 0)
-    RCLCPP_ERROR(logger, "The number of desired planning attempts should be positive. "
+    RCLCPP_ERROR(LOGGER, "The number of desired planning attempts should be positive. "
                                           "Assuming one attempt.");
   request_.num_planning_attempts = std::max(1, request_.num_planning_attempts);
 }
