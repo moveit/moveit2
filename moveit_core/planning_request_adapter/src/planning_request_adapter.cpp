eb550d8e03780d15b425bdf24a084f148c003b93
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-03-31 17:17:21 +0200

    Port planning_request_adapter to ROS 2

--- moveit_core/planning_request_adapter/src/planning_request_adapter.cpp
+++ moveit_core/planning_request_adapter/src/planning_request_adapter.cpp
@@ -42,6 +42,8 @@
 
 namespace planning_request_adapter
 {
+  rclcpp::Logger logger = rclcpp::get_logger("planning_request_adapter");
+
 namespace
 {
 bool callPlannerInterfaceSolve(const planning_interface::PlannerManager* planner,
@@ -91,7 +93,7 @@ bool callAdapter1(const PlanningRequestAdapter* adapter, const planning_interfac
   }
   catch (std::exception& ex)
   {
-    ROS_ERROR_NAMED("planning_request_adapter", "Exception caught executing *final* adapter '%s': %s",
+    RCLCPP_ERROR(logger, "Exception caught executing *final* adapter '%s': %s",
                     adapter->getDescription().c_str(), ex.what());
     added_path_index.clear();
     return callPlannerInterfaceSolve(planner.get(), planning_scene, req, res);
@@ -109,7 +111,7 @@ bool callAdapter2(const PlanningRequestAdapter* adapter, const PlanningRequestAd
   }
   catch (std::exception& ex)
   {
-    ROS_ERROR_NAMED("planning_request_adapter", "Exception caught executing *next* adapter '%s': %s",
+    RCLCPP_ERROR(logger, "Exception caught executing *next* adapter '%s': %s",
                     adapter->getDescription().c_str(), ex.what());
     added_path_index.clear();
     return planner(planning_scene, req, res);
