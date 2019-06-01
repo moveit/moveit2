f9a1bb5b6437bdd920687a4fe23467c3b41cc91e
Alejandro Hernández Cordero <ahcorde@gmail.com>
2019-06-01 02:10:00 +0200

    Improvements in Planning interface port to ROS 2 (#86)
    * Adding node to planning_interface
    
    * moveit_core planning_interface - library as shared
    
    * moveit_core planning_interface - fixing CMakeLists.txt
    
    * clang
    
    * moveit_core planning_interface - update docstrings

--- moveit_core/planning_interface/include/moveit/planning_interface/planning_interface.h
+++ moveit_core/planning_interface/include/moveit/planning_interface/planning_interface.h
@@ -162,10 +162,9 @@ public:
 
   /// Initialize a planner. This function will be called after the construction of the plugin, before any other call is
   /// made.
-  /// It is assumed that motion plans will be computed for the robot described by \e model and that any exposed ROS
-  /// functionality
-  /// or required ROS parameters are namespaced by \e ns
-  virtual bool initialize(const robot_model::RobotModelConstPtr& model, const std::string& ns);
+  /// It is assumed that motion plans will be computed for the robot described by \e model
+  /// and the node is passed as an argument to get some ROS parameters
+  virtual bool initialize(const robot_model::RobotModelConstPtr& model, std::shared_ptr<rclcpp::Node>& node);
 
   /// Get \brief a short string that identifies the planning interface
   virtual std::string getDescription() const;
