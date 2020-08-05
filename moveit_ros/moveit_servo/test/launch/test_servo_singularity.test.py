f85210ec0cc81e0acdc50ea5d2f70d63a9cbcab2
AdamPettinger <adam.pettinger@utexas.edu>
2020-08-05 15:41:14 -0500

    Add singularity checking integration test

---
+++ moveit_ros/moveit_servo/test/launch/test_servo_singularity.test.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import unittest
+
+import launch_testing.asserts
+sys.path.append(os.path.dirname(__file__))
+from servo_launch_test_common import generate_servo_test_description 
+
+def generate_test_description():
+    return generate_servo_test_description(gtest_name='test_servo_singularity',
+                                            start_position_path='../config/singularity_start_positions.yaml')
+
+
+class TestGTestProcessActive(unittest.TestCase):
+
+    def test_gtest_run_complete(self, proc_info, servo_gtest, test_container, fake_joint_driver_node):
+        proc_info.assertWaitForShutdown(servo_gtest, timeout=4000.0)
+
+@launch_testing.post_shutdown_test()
+class TestGTestProcessPostShutdown(unittest.TestCase):
+
+    def test_gtest_pass(self, proc_info, servo_gtest, test_container, fake_joint_driver_node):
+        launch_testing.asserts.assertExitCodes(proc_info, process=servo_gtest)
