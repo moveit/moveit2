2ccb8c393d5f32bf9f4e9e9fba1de38a381244e3
JafarAbdi <cafer.abdi@gmail.com>
2020-08-14 16:04:38 +0000

    Use new moveit_resources layout

--- moveit_demo_nodes/run_moveit_cpp/launch/run_moveit_cpp.launch.py
+++ moveit_demo_nodes/run_moveit_cpp/launch/run_moveit_cpp.launch.py
@@ -30,13 +30,13 @@ def generate_launch_description():
     moveit_cpp_yaml_file_name = get_package_share_directory('run_moveit_cpp') + "/config/moveit_cpp.yaml"
 
     # Component yaml files are grouped in separate namespaces
-    robot_description_config = load_file('moveit_resources', 'panda_description/urdf/panda.urdf')
+    robot_description_config = load_file('moveit_resources_panda_description', 'urdf/panda.urdf')
     robot_description = {'robot_description' : robot_description_config}
 
-    robot_description_semantic_config = load_file('moveit_resources', 'panda_moveit_config/config/panda.srdf')
+    robot_description_semantic_config = load_file('moveit_resources_panda_moveit_config', 'config/panda.srdf')
     robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
 
-    kinematics_yaml = load_yaml('moveit_resources', 'panda_moveit_config/config/kinematics.yaml')
+    kinematics_yaml = load_yaml('moveit_resources_panda_moveit_config', 'config/kinematics.yaml')
     robot_description_kinematics = { 'robot_description_kinematics' : kinematics_yaml }
 
     controllers_yaml = load_yaml('run_moveit_cpp', 'config/controllers.yaml')
@@ -47,7 +47,7 @@ def generate_launch_description():
         'planning_plugin' : 'ompl_interface/OMPLPlanner',
         'request_adapters' : """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""" ,
         'start_state_max_bounds_error' : 0.1 } }
-    ompl_planning_yaml = load_yaml('moveit_resources', 'panda_moveit_config/config/ompl_planning.yaml')
+    ompl_planning_yaml = load_yaml('moveit_resources_panda_moveit_config', 'config/ompl_planning.yaml')
     ompl_planning_pipeline_config['ompl'].update(ompl_planning_yaml)
 
     # MoveItCpp demo executable
