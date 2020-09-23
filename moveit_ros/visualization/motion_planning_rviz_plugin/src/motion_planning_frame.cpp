565d8d789f51a6a267da7921f841e9d0de1371b8
RoboticsYY <yu.yan@intel.com>
2020-09-23 21:35:42 +0800

    Port warehouse part of motion_planning_frame.cpp

--- moveit_ros/visualization/motion_planning_rviz_plugin/src/motion_planning_frame.cpp
+++ moveit_ros/visualization/motion_planning_rviz_plugin/src/motion_planning_frame.cpp
@@ -393,9 +393,8 @@ void MotionPlanningFrame::changePlanningGroupHelper()
 #endif
       move_group_.reset(new moveit::planning_interface::MoveGroupInterface(node_, opt, tf_buffer, rclcpp::Duration(30)));
       // TODO (ddengster): Enable when moveit_ros_warehouse is ported
-      //      if (planning_scene_storage_)
-      //        move_group_->setConstraintsDatabase(ui_->database_host->text().toStdString(),
-      //        ui_->database_port->value());
+      if (planning_scene_storage_)
+        move_group_->setConstraintsDatabase(ui_->database_host->text().toStdString(), ui_->database_port->value());
     }
     catch (std::exception& ex)
     {
