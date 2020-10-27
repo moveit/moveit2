b3913f379cbd022940e67681e105eb8e777b35c3
Sebastian Jahr <sebastian.jahr@picknik.ai>
2020-10-27 09:31:58 +0100

    Fix test launch file and add test yaml parameter file in moveit_kinematics

--- moveit_kinematics/test/fanuc-kdl-test.py
+++ moveit_kinematics/test/fanuc-kdl-test.py
@@ -1,8 +1,10 @@
 import os
 import yaml
+import unittest
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import launch_testing.actions
+import launch_testing
 from ament_index_python.packages import get_package_share_directory
 
 import pytest
@@ -31,82 +33,34 @@ def load_yaml(package_name, file_path):
 def generate_test_description():
 
     # Component yaml files are grouped in separate namespaces
-    robot_description_config = load_file('moveit_resources', 'fanuc_description/urdf/fanuc.urdf')
+    robot_description_config = load_file('moveit_resources_fanuc_description', 'urdf/fanuc.urdf')
     robot_description = {'robot_description' : robot_description_config}
 
-    robot_description_semantic_config = load_file('moveit_resources', 'fanuc_moveit_config/config/fanuc.srdf')
+    robot_description_semantic_config = load_file('moveit_resources_fanuc_moveit_config', 'config/fanuc.srdf')
     robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
-
-    kinematics_yaml = load_yaml('moveit_resources', 'fanuc_moveit_config/config/kinematics.yaml')
-    kinematics_yaml['manipulator']['max_solver_iterations'] = 100 # does this work?
+    kinematics_yaml = load_yaml('moveit_resources_fanuc_moveit_config', 'config/kinematics.yaml')
     robot_description_kinematics = { 'robot_description_kinematics' : kinematics_yaml }
-
-    tolerance = 0.1  ## TODO(henningkayser): set arg
-    test_params = {
-        'tip_link': 'tool0',
-        'root_link': 'base_link',
-        'group': 'manipulator',
-        'ik_timeout': 0.2,
-        'tolerance': tolerance,
-        'joint_names': [ 'joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6' ],
-    }
-
-    ik_plugin = 'kdl_kinematics_plugin/KDLKinematicsPlugin'  # TODO(henningkayser): set arg
-    kdl_params = {
-        'ik_plugin_name': ik_plugin,
-        'num_fk_tests': 100,
-        'num_ik_tests': 100,
-        'num_ik_cb_tests': 0,
-        'num_ik_multipletests': 0,
-        'num_nearest_ik_tests': 0,
-    }
-
-    private_params = {
-        'seed': [0, -0.32, -0.5, 0, -0.5, 0],
-        'consistency_limits': [0.4, 0.4, 0.4, 0.4, 0.4, 0.4],
-    }
-
-    unit_tests_poses = {
-        'unit_test_poses':
-        {
-            'size': 6,
-            'pose_1': { 'pose':   [0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
-                        'joints': [0, -0.152627, -0.367847, 0, -0.46478, 0],
-                        'type':  'relative'
-                      },
-            'pose_2': { 'pose':   [0.0, 0.1, 0.0, 0.0, 0.0, 0.0],
-                        'joints': [0.1582256, -0.3066389, -0.490349, 0.250946, -0.5159858, -0.319381],
-                        'type':   'relative'
-                      },
-            'pose_3': { 'pose':   [0.0, 0.0, 0.1, 0.0, 0.0, 0.0],
-                        'joints': [0, -0.287588, -0.324304, 0, -0.643285, 0],
-                        'type':   'relative'
-                      },
-            'pose_4': { 'pose':   [0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
-                        'joints': [-0.0159181, -0.319276, -0.499953, -0.231014, -0.511806, 0.212341],
-                        'type':   'relative'
-                      },
-            'pose_5': { 'pose':   [0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
-                        'joints': [0, -0.331586, -0.520375, 0, -0.391211, 0],
-                        'type':   'relative'
-                      },
-            'pose_6': { 'pose':   [0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
-                        'joints': [0, -0.32, -0.5, 0, -0.5, -0.1],
-                        'type':   'relative'
-                      },
-        }
-    }
-
+    test_param_yaml = load_yaml('moveit_kinematics', 'config/fanuc-kdl-test.yaml')
+    test_param = {'test_param' : test_param_yaml}
     test_node = Node(package='moveit_kinematics',
-                     node_executable='test_kinematics_plugin',
-                     node_name='test_kinmatics_plugin',
+                     executable='test_kinematics_plugin',
+                     name='test_kinmatics_plugin',
                      # prefix='xterm -e gdb --args',
                      parameters=[robot_description,
                                  robot_description_semantic,
                                  robot_description_kinematics,
-                                 test_params,
-                                 kdl_params,
-                                 private_params])
+                                 test_param
+                                 ]
+    )
+    return LaunchDescription([
+        # other fixture actions
+        test_node,
+        launch_testing.actions.ReadyToTest(),
+    ])
 
-    return LaunchDescription([ test_node, launch_testing.actions.ReadyToTest() ])
+@launch_testing.post_shutdown_test()
+class TestOutcome(unittest.TestCase):
+
+    def test_exit_codes(self, proc_info):
+        launch_testing.asserts.assertExitCodes(proc_info)
 
