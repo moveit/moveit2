8254f23a5d1857bc8086ae2b21230eeca6a5b85b
Henning Kayser <henningkayser@picknik.ai>
2021-02-01 19:51:28 +0100

    Code cleanup & MoveIt best practices (#351)
    * Export missing plugins
    * Use std::chrono_literals
    * Construct smart pointers with std::make_* instead of 'new'
    * Fixup const-ref in function signatures, reduce copies
    * Improve planning scene locking, robot state processing, controller logic

--- moveit_ros/hybrid_planning/local_constraint_solver_plugins/src/decelerate_before_collision.cpp
+++ moveit_ros/hybrid_planning/local_constraint_solver_plugins/src/decelerate_before_collision.cpp
@@ -47,28 +47,32 @@ const double CYLCE_TIME = 0.01;  // TODO(sjahr) Add param and proper time handli
 DecelerateBeforeCollision::DecelerateBeforeCollision() : loop_rate_(1 / CYLCE_TIME){};
 
 bool DecelerateBeforeCollision::initialize(const rclcpp::Node::SharedPtr& node,
-                                           planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor)
+                                           const planning_scene_monitor::PlanningSceneMonitorPtr& planning_scene_monitor,
+                                           const std::string& group_name)
 {
   planning_scene_monitor_ = planning_scene_monitor;
   node_handle_ = node;
   path_invalidation_event_send_ = false;
 
   // Initialize PID
-  auto joint_names = (planning_scene_monitor_->getRobotModel())->getJointModelNames();
-  for (std::size_t i = 0; i < joint_names.size(); i++)
+  const auto jmg = planning_scene_monitor_->getRobotModel()->getJointModelGroup(group_name);
+  for (const auto& joint_name : jmg->getActiveJointModelNames())
   {
-    joint_position_pids_.push_back(control_toolbox::Pid(pid_config_.k_p, pid_config_.k_i, pid_config_.k_d,
-                                                        pid_config_.windup_limit, -pid_config_.windup_limit,
-                                                        true));  // TODO(sjahr) Add ROS2 param
+    joint_position_pids_[joint_name] = control_toolbox::Pid(pid_config_.k_p, pid_config_.k_i, pid_config_.k_d,
+                                                            pid_config_.windup_limit, -pid_config_.windup_limit,
+                                                            true);  // TODO(sjahr) Add ROS2 param
   }
   return true;
 }
 
-trajectory_msgs::msg::JointTrajectory
-DecelerateBeforeCollision::solve(robot_trajectory::RobotTrajectory local_trajectory,
-                                 std::vector<moveit_msgs::msg::Constraints> local_constraints,
-                                 std::shared_ptr<moveit_msgs::action::LocalPlanner::Feedback> feedback)
+moveit_msgs::action::LocalPlanner::Feedback
+DecelerateBeforeCollision::solve(const robot_trajectory::RobotTrajectory& local_trajectory,
+                                 const std::vector<moveit_msgs::msg::Constraints>& local_constraints,
+                                 trajectory_msgs::msg::JointTrajectory& local_solution)
 {
+  // Feedback result
+  moveit_msgs::action::LocalPlanner::Feedback feedback_result;
+
   // Get current planning scene
   planning_scene_monitor_->updateFrameTransforms();
 
@@ -78,7 +82,7 @@ DecelerateBeforeCollision::solve(robot_trajectory::RobotTrajectory local_traject
   std::vector<std::size_t>* invalid_index = nullptr;
 
   // Get Current State
-  moveit::core::RobotState current_state = locked_planning_scene->getCurrentState();
+  const moveit::core::RobotState& current_state = locked_planning_scene->getCurrentState();
 
   // Check if path is valid
   if (locked_planning_scene->isPathValid(local_trajectory, local_trajectory.getGroupName(), false, invalid_index))
@@ -95,7 +99,7 @@ DecelerateBeforeCollision::solve(robot_trajectory::RobotTrajectory local_traject
   {
     if (!path_invalidation_event_send_)
     {  // Send feedback only once
-      feedback->feedback = "collision_ahead";
+      feedback_result.feedback = "collision_ahead";
       path_invalidation_event_send_ = true;  // Set feedback flag
     }
 
@@ -103,39 +107,42 @@ DecelerateBeforeCollision::solve(robot_trajectory::RobotTrajectory local_traject
     robot_command.addSuffixWayPoint(current_state, 0.0);
   }
 
-  // Transform into joint_trajectory
+  // Transform robot trajectory into joint_trajectory message
   moveit_msgs::msg::RobotTrajectory robot_command_msg;
   robot_command.getRobotTrajectoryMsg(robot_command_msg);
-
-  trajectory_msgs::msg::JointTrajectory joint_trajectory = robot_command_msg.joint_trajectory;
+  const auto& joint_trajectory = robot_command_msg.joint_trajectory;
 
   // Transform current state into msg
   moveit_msgs::msg::RobotState current_state_msg;
   robotStateToRobotStateMsg(current_state, current_state_msg);
 
-  // Calculate PID command
-  std::vector<double> delta_theta;
-  for (std::size_t i = 0; i < joint_trajectory.points[0].positions.size(); i++)
-  {
-    double error = joint_trajectory.points[0].positions[i] - current_state_msg.joint_state.position[i];
-    delta_theta.push_back(joint_position_pids_[i].computeCommand(error, loop_rate_.period().count()));
-  }
-
-  // Apply PID command
+  // Initialize command goal
   trajectory_msgs::msg::JointTrajectoryPoint command_goal_point;
   command_goal_point.time_from_start = rclcpp::Duration(loop_rate_.period().count());
-  for (std::size_t i = 0; i < joint_trajectory.points[0].positions.size(); i++)
+
+  // Calculate PID command
+  const auto& target_state = joint_trajectory.points[0];
+  for (std::size_t i = 0; i < target_state.positions.size(); i++)
   {
-    // Increment joint
-    command_goal_point.positions.push_back(current_state_msg.joint_state.position[i] += delta_theta[i]);
+    // Skip position if joint is not part of active group
+    const auto& joint_name = joint_trajectory.joint_names[i];
+    if (joint_position_pids_.find(joint_name) == joint_position_pids_.end())
+      continue;
+
+    // Calculate PID command
+    const double error = target_state.positions[i] - current_state_msg.joint_state.position[i];
+    const double delta_theta = joint_position_pids_[joint_name].computeCommand(error, loop_rate_.period().count());
+
+    // Apply delta to current state to compute goal command
+    command_goal_point.positions.push_back(current_state_msg.joint_state.position[i] += delta_theta);
   }
 
-  // Add new command point to local solution joint trajectory
-  joint_trajectory.points.clear();
-  joint_trajectory.points.push_back(command_goal_point);
+  // Replace local trajectory with goal command
+  local_solution.joint_names = joint_trajectory.joint_names;
+  local_solution.points = { command_goal_point };
 
-  // Return only joint trajectory
-  return joint_trajectory;
+  // Return feedback_result
+  return feedback_result;
 }
 }  // namespace moveit_hybrid_planning
 
