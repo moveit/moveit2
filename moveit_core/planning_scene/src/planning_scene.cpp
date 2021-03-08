62bde8adf96aa7cfa6c3a4ead6a35691fd3eb87a
AndyZe <zelenak@picknik.ai>
2021-03-07 22:42:46 -0600

    Experiment with several methods of PS construction, to fix Servo test

--- moveit_core/planning_scene/src/planning_scene.cpp
+++ moveit_core/planning_scene/src/planning_scene.cpp
@@ -193,6 +193,7 @@ PlanningScene::PlanningScene(const PlanningSceneConstPtr& parent) : parent_(pare
   // record changes to the world
   world_diff_.reset(new collision_detection::WorldDiff(world_));
 
+/*
   // Set up the same collision detector as the parent
   const CollisionDetectorPtr& parent_detector = parent_->collision_detector_;
 
@@ -204,6 +205,20 @@ PlanningScene::PlanningScene(const PlanningSceneConstPtr& parent) : parent_(pare
       collision_detector_->alloc_->allocateEnv(parent_detector->cenv_unpadded_, world_);
   collision_detector_->cenv_unpadded_const_ = collision_detector_->cenv_unpadded_;
   collision_detector_->copyPadding(*parent_detector);
+*/
+
+/*
+  // Set up the same collision detector as the parent
+  collision_detector_.reset(new CollisionDetector());
+  collision_detector_->alloc_ = parent_->collision_detector_->alloc_;
+  collision_detector_->cenv_ = parent_->collision_detector_->alloc_->allocateEnv(world_, getRobotModel());
+  collision_detector_->cenv_const_ = parent_->collision_detector_->cenv_;
+  collision_detector_->cenv_unpadded_ = parent_->collision_detector_->alloc_->allocateEnv(world_, getRobotModel());
+  collision_detector_->cenv_unpadded_const_ = parent_->collision_detector_->cenv_unpadded_;
+  collision_detector_->copyPadding(*parent_->collision_detector_);
+*/
+  setCollisionDetectorType(parent_->collision_detector_->alloc_);
+  collision_detector_->copyPadding(*parent_->collision_detector_);
 }
 
 PlanningScenePtr PlanningScene::clone(const PlanningSceneConstPtr& scene)
