5385094c9d4468989d60dd43e1a9650d4a06c45d
JafarAbdi <cafer.abdi@gmail.com>
2021-03-18 18:20:07 +0000

    Port planning_interface tests to use GenericSystem from ros2_control

--- moveit_ros/planning_interface/test/launch/move_group_launch_test_common.py
+++ moveit_ros/planning_interface/test/launch/move_group_launch_test_common.py
@@ -3,7 +3,9 @@ import yaml
 import launch
 import launch_ros
 import launch_testing
+import xacro
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch.some_substitutions_type import SomeSubstitutionsType
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import ComposableNodeContainer, Node
@@ -44,22 +46,16 @@ def load_test_yaml(absolute_file_path):
 def generate_move_group_test_description(*args, gtest_name: SomeSubstitutionsType):
 
     # planning_context
-    robot_description_config = load_file(
-        "moveit_resources_panda_description", "urdf/panda.urdf"
-    )
-    robot_description = {"robot_description": robot_description_config}
+    robot_description_config = xacro.process_file(os.path.join(get_package_share_directory('moveit_resources_panda_moveit_config'),
+                                                               'config',
+                                                               'panda.urdf.xacro'))
+    robot_description = {'robot_description' : robot_description_config.toxml()}
 
-    robot_description_semantic_config = load_file(
-        "moveit_resources_panda_moveit_config", "config/panda.srdf"
-    )
-    robot_description_semantic = {
-        "robot_description_semantic": robot_description_semantic_config
-    }
+    robot_description_semantic_config = load_file('moveit_resources_panda_moveit_config', 'config/panda.srdf')
+    robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
 
-    kinematics_yaml = load_yaml(
-        "moveit_resources_panda_moveit_config", "config/kinematics.yaml"
-    )
-    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
+    kinematics_yaml = load_yaml('moveit_resources_panda_moveit_config', 'config/kinematics.yaml')
+    robot_description_kinematics = { 'robot_description_kinematics' : kinematics_yaml }
 
     # Planning Functionality
     ompl_planning_pipeline_config = {
@@ -76,28 +72,20 @@ def generate_move_group_test_description(*args, gtest_name: SomeSubstitutionsTyp
     ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
     # Trajectory Execution Functionality
-    controllers_yaml = load_test_yaml(
-        os.path.join(os.path.dirname(__file__), "../config/controllers.yaml")
-    )
+    moveit_simple_controllers_yaml = load_yaml('moveit_resources_panda_moveit_config', 'config/panda_controllers.yaml')
 
-    moveit_controllers = {
-        "moveit_simple_controller_manager": controllers_yaml,
-        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
-    }
+    moveit_controllers = { 'moveit_simple_controller_manager' : moveit_simple_controllers_yaml,
+                           'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
 
-    trajectory_execution = {
-        "moveit_manage_controllers": True,
-        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
-        "trajectory_execution.allowed_goal_duration_margin": 0.5,
-        "trajectory_execution.allowed_start_tolerance": 0.01,
-    }
+    trajectory_execution = {'moveit_manage_controllers': True,
+                            'trajectory_execution.allowed_execution_duration_scaling': 1.2,
+                            'trajectory_execution.allowed_goal_duration_margin': 0.5,
+                            'trajectory_execution.allowed_start_tolerance': 0.01}
 
-    planning_scene_monitor_parameters = {
-        "publish_planning_scene": True,
-        "publish_geometry_updates": True,
-        "publish_state_updates": True,
-        "publish_transforms_updates": True,
-    }
+    planning_scene_monitor_parameters = {"publish_planning_scene": True,
+                 "publish_geometry_updates": True,
+                 "publish_state_updates": True,
+                 "publish_transforms_updates": True}
 
     # Start the actual move_group node/action server
     run_move_group_node = Node(
@@ -133,17 +121,29 @@ def generate_move_group_test_description(*args, gtest_name: SomeSubstitutionsTyp
         parameters=[robot_description],
     )
 
-    # Fake joint driver
-    fake_joint_driver_node = Node(
-        package="fake_joint_driver",
-        executable="fake_joint_driver_node",
-        parameters=[
-            {"controller_name": "panda_arm_controller"},
-            os.path.join(os.path.dirname(__file__), "../config/panda_controllers.yaml"),
-            os.path.join(os.path.dirname(__file__), "../config/start_positions.yaml"),
-            robot_description,
-        ],
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(get_package_share_directory("moveit_resources_panda_moveit_config"), "config", "panda_ros_controllers.yaml")
+    ros2_control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, ros2_controllers_path],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        },
     )
+
+    # load joint_state_controller
+    load_joint_state_controller = ExecuteProcess(cmd=['ros2 control load_start_controller joint_state_controller'], shell=True, output='screen')
+    load_controllers = [load_joint_state_controller]
+    # load panda_arm_controller
+    load_controllers += [ExecuteProcess(cmd=['ros2 control load_configure_controller panda_arm_controller'],
+                                        shell=True,
+                                        output='screen',
+                                        on_exit=[ExecuteProcess(cmd=['ros2 control switch_controllers --start-controllers panda_arm_controller'],
+                                                                shell=True,
+                                                                output='screen')])]
+
 
     # Warehouse mongodb server
     mongodb_server_node = Node(
@@ -158,34 +158,26 @@ def generate_move_group_test_description(*args, gtest_name: SomeSubstitutionsTyp
     )
 
     # test executable
-    ompl_constraint_test = launch_ros.actions.Node(
-        executable=PathJoinSubstitution(
-            [LaunchConfiguration("test_binary_dir"), gtest_name]
-        ),
-        parameters=[robot_description, robot_description_semantic, kinematics_yaml],
-        output="screen",
-    )
+    ompl_constraint_test = launch_ros.actions.Node(executable=PathJoinSubstitution([LaunchConfiguration('test_binary_dir'), gtest_name]),
+                                                   parameters=[robot_description,
+                                                              robot_description_semantic,
+                                                              kinematics_yaml],
+                                                   output='screen',)
 
-    return launch.LaunchDescription(
-        [
-            launch.actions.DeclareLaunchArgument(
-                name="test_binary_dir",
-                description="Binary directory of package "
-                "containing test executables",
-            ),
-            run_move_group_node,
-            static_tf,
-            robot_state_publisher,
-            fake_joint_driver_node,
-            mongodb_server_node,
-            ompl_constraint_test,
-            launch_testing.actions.ReadyToTest(),
-        ]
-    ), {
-        "run_move_group_node": run_move_group_node,
-        "static_tf": static_tf,
-        "robot_state_publisher": robot_state_publisher,
-        "fake_joint_driver_node": fake_joint_driver_node,
-        "mongodb_server_node": mongodb_server_node,
-        "ompl_constraint_test": ompl_constraint_test,
-    }
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='test_binary_dir',
+                                             description='Binary directory of package '
+                                                         'containing test executables'),
+        run_move_group_node,
+        static_tf,
+        robot_state_publisher,
+        ros2_control_node,
+        mongodb_server_node,
+        ompl_constraint_test,
+        launch_testing.actions.ReadyToTest()
+    ] + load_controllers), {'run_move_group_node': run_move_group_node,
+         'static_tf': static_tf,
+         'robot_state_publisher': robot_state_publisher,
+         'ros2_control_node': ros2_control_node,
+         'mongodb_server_node': mongodb_server_node,
+         'ompl_constraint_test': ompl_constraint_test}
