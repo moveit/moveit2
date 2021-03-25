df58f71c78ee2cee5c71cfa365390107fbce7fa4
Sebastian Jahr <sebastian.jahr@student.kit.edu>
2021-03-25 22:03:57 +0100

    Restructure hybrid_planning package (#426) * Add forward_trajectory local solver plugin (#359) * Use ros2_control based joint simulation and remove unnecessary comment * Update copyrights * Restructure hybrid planning package * Fix formatting and add missing time stamp in local solver plugin * Remove unnecessary logging and param loading * Enable different interfaces between local planner and controller * Use JointGroupPositionController as demo hardware controller

--- moveit_ros/hybrid_planning/test/hybrid_planning_test_node.cpp
+++ moveit_ros/hybrid_planning/test/hybrid_planning_test_node.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2020, PickNik LLC
+ *  Copyright (c) 2020, PickNik Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of PickNik LLC nor the names of its
+ *   * Neither the name of PickNik Inc. nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -81,7 +81,6 @@ public:
     // Add new collision object as soon as global trajectory is available.
     global_solution_subscriber_ = node_->create_subscription<moveit_msgs::msg::MotionPlanResponse>(
         "global_trajectory", 1, [this](const moveit_msgs::msg::MotionPlanResponse::SharedPtr msg) {
-          RCLCPP_INFO(LOGGER, "0");
           // Remove old collision objects
           collision_object_1_.operation = collision_object_1_.REMOVE;
 
