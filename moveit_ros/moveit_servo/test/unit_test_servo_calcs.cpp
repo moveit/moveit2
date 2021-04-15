97ae05b0482a88664418847db05eab785febe6fe
Vatan Aksoy Tezer <vatan@picknik.ai>
2021-04-15 17:02:51 +0000

    Cleanup comments

--- moveit_ros/moveit_servo/test/unit_test_servo_calcs.cpp
+++ moveit_ros/moveit_servo/test/unit_test_servo_calcs.cpp
@@ -360,7 +360,6 @@ TEST_F(ServoCalcsTestFixture, TestEnforceVelLimits)
   for (size_t i = 0; i < 7; ++i)
   {
     // We need to check vs radians-per-loop allowable rate (not rad/s)
-    // was EXPECT_LE
     EXPECT_GE(desired_velocity[i], vel_limits[i] * servo_calcs_->parameters_->publish_period);
   }
 
@@ -371,7 +370,6 @@ TEST_F(ServoCalcsTestFixture, TestEnforceVelLimits)
   for (size_t i = 0; i < 7; ++i)
   {
     // We need to check vs radians-per-loop allowable rate (not rad/s)
-    // was EXPECT_GE
     EXPECT_LE(desired_velocity[i], -1 * vel_limits[i] * servo_calcs_->parameters_->publish_period);
   }
 }
