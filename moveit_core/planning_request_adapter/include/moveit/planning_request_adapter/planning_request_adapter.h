827646fab15dcdbfc91077c69e2cea392dcc1673
David V. Lu!! <davidvlu@gmail.com>
2021-05-27 15:21:56 -0400

    PlanningRequestAdapter helper method getParam()  (#468)
    * Implement parameters for adapter plugins

--- moveit_core/planning_request_adapter/include/moveit/planning_request_adapter/planning_request_adapter.h
+++ moveit_core/planning_request_adapter/include/moveit/planning_request_adapter/planning_request_adapter.h
@@ -90,6 +90,27 @@ public:
                             const planning_interface::MotionPlanRequest& req,
                             planning_interface::MotionPlanResponse& res,
                             std::vector<std::size_t>& added_path_index) const = 0;
+
+protected:
+  /** \brief Helper param for getting a parameter using a namespace **/
+  template <typename T>
+  T getParam(const rclcpp::Node::SharedPtr& node, const rclcpp::Logger& logger, const std::string& parameter_namespace,
+             const std::string& parameter_name, T default_value = {}) const
+  {
+    std::string full_name = parameter_namespace.empty() ? parameter_name : parameter_namespace + "." + parameter_name;
+    T value;
+    if (!node->get_parameter(full_name, value))
+    {
+      RCLCPP_INFO(logger, "Param '%s' was not set. Using default value: %s", full_name.c_str(),
+                  std::to_string(default_value).c_str());
+      return default_value;
+    }
+    else
+    {
+      RCLCPP_INFO(logger, "Param '%s' was set to %s", full_name.c_str(), std::to_string(default_value).c_str());
+      return value;
+    }
+  }
 };
 
 /// Apply a sequence of adapters to a motion plan
