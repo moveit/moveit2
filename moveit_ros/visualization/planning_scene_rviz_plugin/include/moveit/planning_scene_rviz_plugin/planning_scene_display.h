d61357f113a95e0fc67f90fd931206fb24ba9b66
Lior Lustgarten <lilustga@microsoft.com>
2021-06-24 20:46:01 -0700

    Fix for planning scene rviz plugin symbol visibility.

--- moveit_ros/visualization/planning_scene_rviz_plugin/include/moveit/planning_scene_rviz_plugin/planning_scene_display.h
+++ moveit_ros/visualization/planning_scene_rviz_plugin/include/moveit/planning_scene_rviz_plugin/planning_scene_display.h
@@ -67,7 +67,7 @@ class EnumProperty;
 
 namespace moveit_rviz_plugin
 {
-class PLANNING_SCENE_RVIZ_PLUGIN_PUBLIC PlanningSceneDisplay : public rviz_common::Display
+class PLANNING_SCENE_RVIZ_PLUGIN_CORE_PUBLIC PlanningSceneDisplay : public rviz_common::Display
 {
   Q_OBJECT
 
