7cc99c4fc4ccedff3d4e777b41a5058fe29c333a
Akash Munagala <akash.munagala@gmail.com>
2021-06-26 16:45:01 -0500

    Remove extra args for boost linking

--- moveit_core/ConfigExtras.cmake
+++ moveit_core/ConfigExtras.cmake
@@ -1,10 +1,5 @@
 # Extras module needed for dependencies to find boost components
 
-if(WIN32)
-  # Fix linking errors on windows
-  add_definitions(-DBOOST_ALL_NO_LIB)
-  add_definitions(-DBOOST_ALL_DYN_LINK)
-endif()
 find_package(Boost REQUIRED
   chrono
   date_time
