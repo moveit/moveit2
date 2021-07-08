dd33b8bfb49a52ae9370b1d11506afd69a2c5fb2
nkalupahana <avishkank@gmail.com>
2021-07-08 10:57:21 -0700

    fixes for bullet

--- moveit_core/CMakeModules/FindBULLET.cmake
+++ moveit_core/CMakeModules/FindBULLET.cmake
@@ -6,5 +6,5 @@ if(PKGCONFIG_FOUND)
 endif()
 
 find_package_handle_standard_args(BULLET
-                                  REQUIRED_VARS BULLET_LIBRARIES BULLET_INCLUDE_DIRS
+                                  REQUIRED_VARS BULLET_LIBRARIES BULLET_INCLUDE_DIRS BULLET_LIBRARY_DIRS
                                   VERSION_VAR BULLET_VERSION)
