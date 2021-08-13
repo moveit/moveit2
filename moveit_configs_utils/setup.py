c08574c4592a6c8722f3d2bfde51a1aed59fa4b4
JafarAbdi <cafer.abdi@gmail.com>
2021-08-14 02:23:02 +0300

    Fixes

--- moveit_configs_utils/setup.py
+++ moveit_configs_utils/setup.py
@@ -5,7 +5,6 @@ package_name = "moveit_configs_utils"
 setup(
     name=package_name,
     version="0.0.0",
-    package_dir={"": "src"},
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
@@ -13,17 +12,13 @@ setup(
     ],
     install_requires=["setuptools"],
     zip_safe=True,
-    author="TODO",
-    author_email="TODO",
-    maintainer="TODO",
-    maintainer_email="TODO",
-    keywords=["ROS2"],
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-    ],
-    description="TODO",
+    maintainer="MoveIt Release Team",
+    maintainer_email="moveit_releasers@googlegroups.com",
+    description="Python library for loading moveit config parameters in launch files",
     license="BSD",
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
