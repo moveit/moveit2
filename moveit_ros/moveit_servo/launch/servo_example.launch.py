1791baf44e074869f8422c3553c55cb176391508
Joe Schornak <joe.schornak@gmail.com>
2021-09-07 10:54:39 -0400

    add launch file to run servo node

---
+++ moveit_ros/moveit_servo/launch/servo_example.launch.py
@@ -0,0 +1,150 @@
+import os
+import yaml
+import launch
+import launch_ros
+from launch import LaunchDescription
+from launch.some_substitutions_type import SomeSubstitutionsType
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def generate_launch_description():
+
+    # Get parameters using the demo config file
+    servo_yaml = load_yaml("moveit_servo", "config/panda_simulated_config.yaml")
+    servo_params = {"moveit_servo": servo_yaml}
+
+    # Get URDF and SRDF
+    if start_position_path:
+        initial_positions_file = os.path.join(
+            os.path.dirname(__file__), start_position_path
+        )
+        robot_description_config = xacro.process_file(
+            os.path.join(
+                get_package_share_directory("moveit_resources_panda_moveit_config"),
+                "config",
+                "panda.urdf.xacro",
+            ),
+            mappings={"initial_positions_file": initial_positions_file},
+        )
+    else:
+        robot_description_config = xacro.process_file(
+            os.path.join(
+                get_package_share_directory("moveit_resources_panda_moveit_config"),
+                "config",
+                "panda.urdf.xacro",
+            )
+        )
+
+    robot_description = {"robot_description": robot_description_config.toxml()}
+
+    robot_description_semantic_config = load_file(
+        "moveit_resources_panda_moveit_config", "config/panda.srdf"
+    )
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_config
+    }
+    joint_limits_yaml = {
+        "robot_description_planning": load_yaml(
+            "moveit_resources_panda_moveit_config", "config/joint_limits.yaml"
+        )
+    }
+
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("moveit_resources_panda_moveit_config"),
+        "config",
+        "panda_ros_controllers.yaml",
+    )
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, ros2_controllers_path],
+        output={
+            "stdout": "screen",
+            "stderr": "screen",
+        },
+    )
+
+    # Load controllers
+    load_controllers = []
+    for controller in ["panda_arm_controller", "joint_state_broadcaster"]:
+        load_controllers += [
+            ExecuteProcess(
+                cmd=["ros2 run controller_manager spawner.py {}".format(controller)],
+                shell=True,
+                output="screen",
+            )
+        ]
+
+    # Component nodes for tf and Servo
+    component_container = ComposableNodeContainer(
+        name="test_servo_integration_container",
+        namespace="/",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="robot_state_publisher",
+                plugin="robot_state_publisher::RobotStatePublisher",
+                name="robot_state_publisher",
+                parameters=[robot_description],
+            ),
+            ComposableNode(
+                package="tf2_ros",
+                plugin="tf2_ros::StaticTransformBroadcasterNode",
+                name="static_tf2_broadcaster",
+                parameters=[{"/child_frame_id": "panda_link0", "/frame_id": "world"}],
+            ),
+        ],
+        output="screen",
+    )
+
+    servo_node = Node(
+        package="moveit_servo",
+        executable="servo_server_node",
+        parameters=[
+            servo_params,
+            robot_description,
+            robot_description_semantic,
+            joint_limits_yaml,
+        ],
+        output={
+            "stdout": "screen",
+            "stderr": "screen",
+        },
+    )
+
+    return launch.LaunchDescription(
+        [
+            ros2_control_node,
+            servo_node,
+            component_container,
+        ]
+        + load_controllers
+    )
