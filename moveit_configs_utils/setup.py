f1fe62dc25ad88f39398a59d925c966f0ebd521d
JafarAbdi <cafer.abdi@gmail.com>
2021-09-09 22:16:03 +0300

    Fixes

--- moveit_configs_utils/setup.py
+++ moveit_configs_utils/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 package_name = "moveit_configs_utils"
 
 setup(
     name=package_name,
     version="0.0.0",
-    packages=find_packages(),
+    packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
