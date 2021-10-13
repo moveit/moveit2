a3602405801b9f4443fb8474b2d1a5905e8d191a
Jafar Abdi <cafer.abdi@gmail.com>
2021-10-13 21:02:20 +0300

    common_objects: getSharedRobotModelLoader fix deadlock (#734)

--- moveit_ros/planning_interface/common_planning_interface_objects/src/common_objects.cpp
+++ moveit_ros/planning_interface/common_planning_interface_objects/src/common_objects.cpp
@@ -56,7 +56,7 @@ struct SharedStorage
     robot_model_loaders_.clear();
   }
 
-  boost::mutex lock_;
+  std::recursive_mutex lock_;
   std::weak_ptr<tf2_ros::Buffer> tf_buffer_;
   std::map<std::string, moveit::core::RobotModelWeakPtr> models_;
   std::map<std::string, CurrentStateMonitorWeakPtr> state_monitors_;
@@ -100,7 +100,7 @@ namespace planning_interface
 std::shared_ptr<tf2_ros::Buffer> getSharedTF()
 {
   SharedStorage& s = getSharedStorage();
-  boost::mutex::scoped_lock slock(s.lock_);
+  std::unique_lock<std::recursive_mutex> slock(s.lock_);
 
   typedef CoupledDeleter<tf2_ros::Buffer, tf2_ros::TransformListener> Deleter;
   std::shared_ptr<tf2_ros::Buffer> buffer = s.tf_buffer_.lock();
@@ -118,7 +118,7 @@ robot_model_loader::RobotModelLoaderPtr getSharedRobotModelLoader(const rclcpp::
                                                                   const std::string& robot_description)
 {
   SharedStorage& s = getSharedStorage();
-  boost::mutex::scoped_lock slock(s.lock_);
+  std::unique_lock<std::recursive_mutex> slock(s.lock_);
   auto it = s.robot_model_loaders_
                 .insert(std::make_pair(node->get_fully_qualified_name() + robot_description,
                                        robot_model_loader::RobotModelLoaderWeakPtr()))
@@ -136,7 +136,7 @@ moveit::core::RobotModelConstPtr getSharedRobotModel(const rclcpp::Node::SharedP
                                                      const std::string& robot_description)
 {
   SharedStorage& s = getSharedStorage();
-  boost::mutex::scoped_lock slock(s.lock_);
+  std::unique_lock<std::recursive_mutex> slock(s.lock_);
   auto it = s.models_.insert(std::make_pair(robot_description, moveit::core::RobotModelWeakPtr())).first;
   moveit::core::RobotModelPtr model = it->second.lock();
   if (!model)
@@ -154,7 +154,7 @@ CurrentStateMonitorPtr getSharedStateMonitor(const rclcpp::Node::SharedPtr& node
                                              const std::shared_ptr<tf2_ros::Buffer>& tf_buffer)
 {
   SharedStorage& s = getSharedStorage();
-  boost::mutex::scoped_lock slock(s.lock_);
+  std::unique_lock<std::recursive_mutex> slock(s.lock_);
   auto it = s.state_monitors_.insert(std::make_pair(robot_model->getName(), CurrentStateMonitorWeakPtr())).first;
   CurrentStateMonitorPtr monitor = it->second.lock();
   if (!monitor)
