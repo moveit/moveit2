46c70521c8363fa88cb46cc897246de125ba1997
AndyZe <zelenak@picknik.ai>
2021-10-13 21:16:38 -0500

    Find/replace deprecated spawner.py

--- moveit_ros/moveit_servo/launch/servo_example.launch.py
+++ moveit_ros/moveit_servo/launch/servo_example.launch.py
@@ -88,7 +88,7 @@ def generate_launch_description():
     for controller in ["panda_arm_controller", "joint_state_broadcaster"]:
         load_controllers += [
             ExecuteProcess(
-                cmd=["ros2 run controller_manager spawner.py {}".format(controller)],
+                cmd=["ros2 run controller_manager spawner {}".format(controller)],
                 shell=True,
                 output="screen",
             )
