e45085d2c1193c266ea4e411d98af78738b0504a
Kaustubh <kaustubh08.ugcs20@iiitranchi.ac.in>
2021-12-22 23:53:53 +0530

    Update moveit_ros/visualization/rviz_plugin_render_tools/src/octomap_render.cpp
    Co-authored-by: Tyler Weaver <tylerjw@gmail.com>

--- moveit_ros/visualization/rviz_plugin_render_tools/src/octomap_render.cpp
+++ moveit_ros/visualization/rviz_plugin_render_tools/src/octomap_render.cpp
@@ -169,7 +169,7 @@ void OcTreeRender::octreeDecoding(const std::shared_ptr<const octomap::OcTree>& 
       bool display_voxel = false;
 
       // the left part evaluates to 1 for free voxels and 2 for occupied voxels
-      if ((static_cast<int>(octree->isNodeOccupied(*it) + 1)) & render_mode_mask)
+      if ((static_cast<int>(octree->isNodeOccupied(*it)) + 1) & render_mode_mask)
       {
         // check if current voxel has neighbors on all sides -> no need to be displayed
         bool all_neighbors_found = true;
