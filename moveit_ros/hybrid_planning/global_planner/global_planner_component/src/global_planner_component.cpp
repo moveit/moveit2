6141e3ae147c58f56f7c455ed93c96c02ac20462
AndyZe <zelenak@picknik.ai>
2022-01-02 08:19:15 -0600

    Put hybrid planning actions under a common namespace (#932)
    * Put hybrid planning actions under a common namespace
    
    * Use ~
    
    * New pkg for common resources. Does not work.
    
    * Use inline rather than anonymous namespace
    
    Co-authored-by: Jafar Abdi <cafer.abdi@gmail.com>
    
    * Revert "Use inline rather than anonymous namespace"
    
    This reverts commit 29a7d279776be21f4666c7e0fadeaa6b7ef8debf.
    
    * Revert "New pkg for common resources. Does not work."
    
    This reverts commit 68a173baee4b7f8b2c1f74285f96c8e3892c5909.
    
    * Some progress toward loading common parameters
    
    Co-authored-by: Jafar Abdi <cafer.abdi@gmail.com>

--- moveit_ros/hybrid_planning/global_planner/global_planner_component/src/global_planner_component.cpp
+++ moveit_ros/hybrid_planning/global_planner/global_planner_component/src/global_planner_component.cpp
@@ -43,7 +43,7 @@
 namespace
 {
 const rclcpp::Logger LOGGER = rclcpp::get_logger("global_planner_component");
-}
+}  // namespace
 
 namespace moveit::hybrid_planning
 {
@@ -62,8 +62,16 @@ GlobalPlannerComponent::GlobalPlannerComponent(const rclcpp::NodeOptions& option
 bool GlobalPlannerComponent::initializeGlobalPlanner()
 {
   // Initialize global planning request action server
+  std::string global_planning_action_name = "";
+  node_->declare_parameter("global_planning_action_name", "");
+  node_->get_parameter<std::string>("global_planning_action_name", global_planning_action_name);
+  if (global_planning_action_name.empty())
+  {
+    RCLCPP_ERROR(LOGGER, "global_planning_action_name was not defined");
+    return false;
+  }
   global_planning_request_server_ = rclcpp_action::create_server<moveit_msgs::action::GlobalPlanner>(
-      node_, "global_planning_action",
+      node_, global_planning_action_name,
       [](const rclcpp_action::GoalUUID& /*unused*/,
          std::shared_ptr<const moveit_msgs::action::GlobalPlanner::Goal> /*unused*/) {
         RCLCPP_INFO(LOGGER, "Received global planning goal request");
