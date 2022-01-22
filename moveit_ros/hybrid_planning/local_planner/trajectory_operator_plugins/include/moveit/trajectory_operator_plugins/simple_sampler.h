b0e2cd0272ae2d99ffb6fd4df0f592caf930c327
AndyZe <zelenak@picknik.ai>
2022-01-22 09:04:02 -0600

    [hybrid planning] Delete the pass-through option (#986)
    * Delete the pass-through option
    
    * Suppress clang warning
    
    * Handle (waypoint_count < 0) possibility
    
    Co-authored-by: Tyler Weaver <tyler@picknik.ai>

--- moveit_ros/hybrid_planning/local_planner/trajectory_operator_plugins/include/moveit/trajectory_operator_plugins/simple_sampler.h
+++ moveit_ros/hybrid_planning/local_planner/trajectory_operator_plugins/include/moveit/trajectory_operator_plugins/simple_sampler.h
@@ -49,20 +49,19 @@ public:
   SimpleSampler() = default;
   ~SimpleSampler() = default;
 
-  bool initialize(const rclcpp::Node::SharedPtr& node, const moveit::core::RobotModelConstPtr& robot_model,
-                  const std::string& group_name) override;
+  bool initialize([[maybe_unused]] const rclcpp::Node::SharedPtr& node,
+                  const moveit::core::RobotModelConstPtr& robot_model, const std::string& group_name) override;
   moveit_msgs::action::LocalPlanner::Feedback
   addTrajectorySegment(const robot_trajectory::RobotTrajectory& new_trajectory) override;
   moveit_msgs::action::LocalPlanner::Feedback
   getLocalTrajectory(const moveit::core::RobotState& current_state,
                      robot_trajectory::RobotTrajectory& local_trajectory) override;
-  double getTrajectoryProgress(const moveit::core::RobotState& current_state) override;
+  double getTrajectoryProgress([[maybe_unused]] const moveit::core::RobotState& current_state) override;
   bool reset() override;
 
 private:
   std::size_t
       next_waypoint_index_;  // Indicates which reference trajectory waypoint is the current local goal constrained
-  bool pass_through_;  // If true, the reference_trajectory is simply forwarded each time the getLocalTrajectory() function is called
   moveit_msgs::action::LocalPlanner::Feedback feedback_;  // Empty feedback
   trajectory_processing::TimeOptimalTrajectoryGeneration time_parametrization_;
   const moveit::core::JointModelGroup* joint_group_;
