682cad30ed33cadfe6e104bfc4116e3508e7f3c8
AndyZe <zelenak@picknik.ai>
2022-02-07 15:57:53 -0600

    Prevent an issue with empty local trajectory

--- moveit_ros/hybrid_planning/local_planner/trajectory_operator_plugins/src/simple_sampler.cpp
+++ moveit_ros/hybrid_planning/local_planner/trajectory_operator_plugins/src/simple_sampler.cpp
@@ -88,21 +88,15 @@ SimpleSampler::getLocalTrajectory(const moveit::core::RobotState& current_state,
     return feedback_;
   }
 
-  // Some other component has flagged not to move to the next waypoint.
-  // Stay put, keep the same local_trajectory.
-  if (prevent_forward_progress_)
-  {
-    return feedback_;
-  }
-
   // Delete previous local trajectory
   local_trajectory.clear();
 
   // Get next desired robot state
   const moveit::core::RobotState next_desired_goal_state = reference_trajectory_->getWayPoint(next_waypoint_index_);
 
-  // Check if state is reached
-  if (next_desired_goal_state.distance(current_state, joint_group_) <= WAYPOINT_RADIAN_TOLERANCE)
+  // Progress to the next waypoint?
+  if (!prevent_forward_progress_ &&
+      next_desired_goal_state.distance(current_state, joint_group_) <= WAYPOINT_RADIAN_TOLERANCE)
   {
     // Update index (and thus desired robot state)
     next_waypoint_index_ = std::min(next_waypoint_index_ + 1, reference_trajectory_->getWayPointCount() - 1);
