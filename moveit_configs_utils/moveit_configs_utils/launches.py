7f04091ff30275ea56321cb5a5c5a653faf77cce
David V. Lu!! <davidvlu@gmail.com>
2022-04-19 08:18:28 -0400

    move_group launch for moveit_configs_utils (#1131)

--- moveit_configs_utils/moveit_configs_utils/launches.py
+++ moveit_configs_utils/moveit_configs_utils/launches.py
@@ -4,6 +4,7 @@ from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 from srdfdom.srdf import SRDF
 
@@ -37,7 +38,7 @@ def generate_rsp_launch(moveit_config):
     return ld
 
 
-def generate_rviz_launch(moveit_config):
+def generate_moveit_rviz_launch(moveit_config):
     """Launch file for rviz"""
     ld = LaunchDescription()
 
@@ -67,7 +68,7 @@ def generate_rviz_launch(moveit_config):
     return ld
 
 
-def generate_assistant_launch(moveit_config):
+def generate_setup_assistant_launch(moveit_config):
     """Launch file for MoveIt Setup Assistant"""
     ld = LaunchDescription()
 
@@ -125,7 +126,7 @@ def generate_spawn_controllers_launch(moveit_config):
     return ld
 
 
-def generate_warehouse_launch(moveit_config):
+def generate_warehouse_db_launch(moveit_config):
     """Launch file for warehouse database"""
     ld = LaunchDescription()
     ld.add_action(
@@ -176,4 +177,57 @@ def generate_warehouse_launch(moveit_config):
     )
     ld.add_action(reset_node)
 
+    return ld
+
+
+def generate_move_group_launch(moveit_config):
+    ld = LaunchDescription()
+
+    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
+    ld.add_action(
+        DeclareBooleanLaunchArg("allow_trajectory_execution", default_value=True)
+    )
+    ld.add_action(
+        DeclareBooleanLaunchArg("publish_monitored_planning_scene", default_value=True)
+    )
+    # load non-default MoveGroup capabilities (space separated)
+    ld.add_action(DeclareLaunchArgument("capabilities", default_value=""))
+    # inhibit these default MoveGroup capabilities (space separated)
+    ld.add_action(DeclareLaunchArgument("disable_capabilities", default_value=""))
+
+    should_publish = LaunchConfiguration("publish_monitored_planning_scene")
+
+    move_group_configuration = {
+        "publish_robot_description_semantic": True,
+        "allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
+        # Note: Wrapping the following values is necessary so that the parameter value can be the empty string
+        "capabilities": ParameterValue(
+            LaunchConfiguration("capabilities"), value_type=str
+        ),
+        "disable_capabilities": ParameterValue(
+            LaunchConfiguration("disable_capabilities"), value_type=str
+        ),
+        # Publish the planning scene of the physical robot so that rviz plugin can know actual robot
+        "publish_planning_scene": should_publish,
+        "publish_geometry_updates": should_publish,
+        "publish_state_updates": should_publish,
+        "publish_transforms_updates": should_publish,
+    }
+
+    move_group_params = [
+        move_group_configuration,
+        moveit_config.to_dict(),
+    ]
+
+    add_debuggable_node(
+        ld,
+        package="moveit_ros_move_group",
+        executable="move_group",
+        commands_file=str(moveit_config.package_path / "launch" / "gdb_settings.gdb"),
+        output="screen",
+        parameters=move_group_params,
+        extra_debug_args=["--debug"],
+        # Set the display variable, in case OpenGL code is used internally
+        additional_env={"DISPLAY": ":0"},
+    )
     return ld
