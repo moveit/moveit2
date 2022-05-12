ba1ddc766e7a0704f1b985ef2486764b30fb63da
David V. Lu!! <davidvlu@gmail.com>
2022-05-12 18:02:02 -0400

    [MSA] Merge main into feature/msa (Part II) (#1240)

--- moveit_setup_assistant/old_assistant/src/tools/moveit_config_data.cpp
+++ moveit_setup_assistant/old_assistant/src/tools/moveit_config_data.cpp
@@ -300,6 +300,55 @@ bool MoveItConfigData::outputSTOMPPlanningYAML(const std::string& file_path)
 }
 
 // ******************************************************************************************
+// Output kinematic config files
+// ******************************************************************************************
+bool MoveItConfigData::outputKinematicsYAML(const std::string& file_path)
+{
+  YAML::Emitter emitter;
+  emitter << YAML::BeginMap;
+
+  // Output every group and the kinematic solver it can use ----------------------------------
+  for (srdf::Model::Group& group : srdf_->groups_)
+  {
+    // Only save kinematic data if the solver is not "None"
+    if (group_meta_data_[group.name_].kinematics_solver_.empty() ||
+        group_meta_data_[group.name_].kinematics_solver_ == "None")
+      continue;
+
+    emitter << YAML::Key << group.name_;
+    emitter << YAML::Value << YAML::BeginMap;
+
+    // Kinematic Solver
+    emitter << YAML::Key << "kinematics_solver";
+    emitter << YAML::Value << group_meta_data_[group.name_].kinematics_solver_;
+
+    // Search Resolution
+    emitter << YAML::Key << "kinematics_solver_search_resolution";
+    emitter << YAML::Value << group_meta_data_[group.name_].kinematics_solver_search_resolution_;
+
+    // Solver Timeout
+    emitter << YAML::Key << "kinematics_solver_timeout";
+    emitter << YAML::Value << group_meta_data_[group.name_].kinematics_solver_timeout_;
+
+    emitter << YAML::EndMap;
+  }
+
+  emitter << YAML::EndMap;
+
+  std::ofstream output_stream(file_path.c_str(), std::ios_base::trunc);
+  if (!output_stream.good())
+  {
+    RCLCPP_ERROR_STREAM(LOGGER, "Unable to open file for writing " << file_path);
+    return false;
+  }
+
+  output_stream << emitter.c_str();
+  output_stream.close();
+
+  return true;  // file created successfully
+}
+
+// ******************************************************************************************
 // Helper function to get the controller that is controlling the joint
 // ******************************************************************************************
 std::string MoveItConfigData::getJointHardwareInterface(const std::string& joint_name)
