7e7385894e509188b0a63b659eb3ec29cd99a342
Jafar <cafer.abdi@gmail.com>
2022-05-16 18:52:23 +0300

    Fix bug in applying planning scene diffs that have attached collision objects (#3124)
    * Add test that trigger the bug in applying scene that have robot state
    diffs
    
    * Fix a bug in planning scene diffs where having applying two diffs would cause them to override the aco
    
    * Fix a bug where removing an attached collision object would keep it in the scene
    
    * Update moveit_core/planning_scene/test/test_planning_scene.cpp
    
    Co-authored-by: AndyZe <andyz@utexas.edu>
    
    * Update moveit_core/planning_scene/test/test_planning_scene.cpp
    
    Co-authored-by: AndyZe <andyz@utexas.edu>
    
    * Update moveit_core/planning_scene/src/planning_scene.cpp
    
    Co-authored-by: AndyZe <andyz@utexas.edu>
    
    * Check parent pointer
    
    Co-authored-by: AndyZe <andyz@utexas.edu>

--- moveit_core/planning_scene/src/planning_scene.cpp
+++ moveit_core/planning_scene/src/planning_scene.cpp
@@ -552,8 +552,8 @@ void PlanningScene::getPlanningSceneDiffMsg(moveit_msgs::msg::PlanningScene& sce
   else
   {
     scene_msg.robot_state = moveit_msgs::msg::RobotState();
-    scene_msg.robot_state.is_diff = true;
   }
+  scene_msg.robot_state.is_diff = true;
 
   if (acm_)
     acm_->getMessage(scene_msg.allowed_collision_matrix);
@@ -610,6 +610,18 @@ void PlanningScene::getPlanningSceneDiffMsg(moveit_msgs::msg::PlanningScene& sce
     }
     if (do_omap)
       getOctomapMsg(scene_msg.world.octomap);
+  }
+
+  // Ensure any detached collision objects get detached from the parent planning scene, too
+  for (const auto& collision_object : scene_msg.world.collision_objects)
+  {
+    if (parent_ && parent_->getCurrentState().hasAttachedBody(collision_object.id))
+    {
+      moveit_msgs::msg::AttachedCollisionObject aco;
+      aco.object.id = collision_object.id;
+      aco.object.operation = moveit_msgs::msg::CollisionObject::REMOVE;
+      scene_msg.robot_state.attached_collision_objects.push_back(aco);
+    }
   }
 }
 
