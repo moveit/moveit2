245cad5566af4ee0e367028773ac73d3974e3a67
Henry Moore <henrygerardmoore@gmail.com>
2022-06-08 16:58:33 -0600

    made thread joining safer

--- moveit_ros/robot_interaction/test/locked_robot_state_test.cpp
+++ moveit_ros/robot_interaction/test/locked_robot_state_test.cpp
@@ -532,9 +532,9 @@ static void runThreads(int ncheck, int nset, int nmod)
   // wait for all threads to finish
   for (int i = 0; i < p; ++i)
   {
-    threads[i]->join();
+    if (threads[i]->joinable()) {threads[i]->join();}
   }
-  wthread.join();
+  if (wthread.joinable()) {wthread.join();}
 
   // clean up
   for (int i = 0; i < p; ++i)
