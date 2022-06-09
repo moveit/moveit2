fff4fdde22c3f72a4c04b2d4815e947407cede28
Henry Moore <henrygerardmoore@gmail.com>
2022-06-09 15:22:44 -0600

    fixed clang format

--- moveit_ros/manipulation/pick_place/src/approach_and_translate_stage.cpp
+++ moveit_ros/manipulation/pick_place/src/approach_and_translate_stage.cpp
@@ -147,9 +147,9 @@ bool executeAttachObject(const ManipulationPlanSharedDataConstPtr& shared_plan_d
     ok = ps->processAttachedCollisionObjectMsg(msg);
   }
   motion_plan.planning_scene_monitor_->triggerSceneUpdateEvent(
-      (planning_scene_monitor::PlanningSceneMonitor::SceneUpdateType)(
-          planning_scene_monitor::PlanningSceneMonitor::UPDATE_GEOMETRY +
-          planning_scene_monitor::PlanningSceneMonitor::UPDATE_STATE));
+      (planning_scene_monitor::PlanningSceneMonitor::
+           SceneUpdateType)(planning_scene_monitor::PlanningSceneMonitor::UPDATE_GEOMETRY +
+                            planning_scene_monitor::PlanningSceneMonitor::UPDATE_STATE));
   return ok;
 }
 
