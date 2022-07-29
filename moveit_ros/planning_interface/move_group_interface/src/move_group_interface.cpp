cb2d41b2b752cd6e47a97474c3e245175c7a0d87
tbastiaens-riwo <105370144+tbastiaens-riwo@users.noreply.github.com>
2022-07-29 16:55:13 +0200

    Changed 'return false' in plan, move and execute such that MoveItErrorCode is returned (#1266)
    (cherry picked from commit 59ba9419df0c4490cb219bd698ba19f90a1334ec)

--- moveit_ros/planning_interface/move_group_interface/src/move_group_interface.cpp
+++ moveit_ros/planning_interface/move_group_interface/src/move_group_interface.cpp
@@ -803,7 +803,7 @@ public:
     if (code != rclcpp_action::ResultCode::SUCCEEDED)
     {
       RCLCPP_ERROR_STREAM(LOGGER, "MoveGroupInterface::plan() failed or timeout reached");
-      return false;
+      return res->error_code;
     }
 
     plan.trajectory_ = res->planned_trajectory;
@@ -883,7 +883,6 @@ public:
     if (code != rclcpp_action::ResultCode::SUCCEEDED)
     {
       RCLCPP_ERROR_STREAM(LOGGER, "MoveGroupInterface::move() failed or timeout reached");
-      return false;
     }
     return res->error_code;
   }
@@ -953,7 +952,6 @@ public:
     if (code != rclcpp_action::ResultCode::SUCCEEDED)
     {
       RCLCPP_ERROR_STREAM(LOGGER, "MoveGroupInterface::execute() failed or timeout reached");
-      return false;
     }
     return res->error_code;
   }
