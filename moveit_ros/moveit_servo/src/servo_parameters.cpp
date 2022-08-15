52a77b790a13fa38f6934eae0d5ff8d3c67f77d6
AndyZe <andyz@utexas.edu>
2022-08-15 09:23:16 -0500

    Mention "deprecation" in the warning

--- moveit_ros/moveit_servo/src/servo_parameters.cpp
+++ moveit_ros/moveit_servo/src/servo_parameters.cpp
@@ -335,7 +335,7 @@ ServoParameters ServoParameters::get(const std::string& ns,
   else
   {
     parameters.leaving_singularity_threshold_multiplier = 1.0;
-    RCLCPP_WARN(LOGGER, "Using default servo singularity handling; add parameter "
+    RCLCPP_WARN(LOGGER, "Using the deprecated type of servo singularity handling; add parameter "
                         "'leaving_singularity_threshold_multiplier' to define leaving singularity threshold. See "
                         "https://github.com/ros-planning/moveit2/pull/620 for more information.");
   }
