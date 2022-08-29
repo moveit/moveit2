18806d2ec6176b4d057476147a4fae00c713f92a
AndyZe <zelenak@picknik.ai>
2022-08-29 13:58:15 -0500

    [Servo] Use a WallRate so the clock is monotonically increasing

--- moveit_ros/moveit_servo/src/servo_calcs.cpp
+++ moveit_ros/moveit_servo/src/servo_calcs.cpp
@@ -327,7 +327,7 @@ void ServoCalcs::stop()
 
 void ServoCalcs::mainCalcLoop()
 {
-  rclcpp::Rate rate(1.0 / parameters_->publish_period);
+  rclcpp::WallRate rate(1.0 / parameters_->publish_period);
 
   while (rclcpp::ok() && !stop_requested_)
   {
