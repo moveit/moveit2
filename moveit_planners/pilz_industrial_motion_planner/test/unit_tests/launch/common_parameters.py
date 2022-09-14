74eb3ce000ed3c2e20c0462802d357652aeb0020
Abishalini <abi.gpuram@gmail.com>
2022-09-14 14:18:33 -0600

    Specify moveit controllers YAML

--- moveit_planners/pilz_industrial_motion_planner/test/unit_tests/launch/common_parameters.py
+++ moveit_planners/pilz_industrial_motion_planner/test/unit_tests/launch/common_parameters.py
@@ -14,6 +14,7 @@ def load_moveit_config():
     description_xacro_file = "urdf/prbt.xacro"
     robot_description_semantic_file = "config/prbt.srdf.xacro"
     robot_description_kinematics_file = "config/kinematics.yaml"
+    moveit_controllers_file = "config/prbt_controllers.yaml"
     joint_limits_file = "config/joint_limits.yaml"
     pilz_cartesian_limits_file = "config/pilz_cartesian_limits.yaml"
 
@@ -26,6 +27,7 @@ def load_moveit_config():
         )
         .robot_description_semantic(file_path=robot_description_semantic_file)
         .robot_description_kinematics(file_path=robot_description_kinematics_file)
+        .trajectory_execution(file_path=moveit_controllers_file)
         .joint_limits(file_path=joint_limits_file)
         .pilz_cartesian_limits(file_path=pilz_cartesian_limits_file)
         .to_moveit_configs()
