d6fb4bfe0c99ecf2d6337dde275db4e0e8f2b9d6
Abishalini <abi.gpuram@gmail.com>
2022-09-15 13:29:54 -0600

    Remove some redundant code

--- moveit_planners/pilz_industrial_motion_planner/test/unit_tests/launch/unittest_joint_limits_aggregator.test.py
+++ moveit_planners/pilz_industrial_motion_planner/test/unit_tests/launch/unittest_joint_limits_aggregator.test.py
@@ -1,7 +1,7 @@
 import launch_testing
 import pytest
 import unittest
-from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_testing.util import KeepAliveProc
