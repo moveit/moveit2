66a64b4a72b6ddef1af2329f20ed8162554d5bcb
AndyZe <zelenak@picknik.ai>
2022-10-31 11:17:24 -0500

    Planning request adapters: short-circuit if failure, return code rather than bool (#1605)
    * Return code rather than bool
    
    * Remove all debug prints
    
    * Small fixup
    
    * Minor cleanup of comment and error handling
    
    * void return from PlannerFn
    
    * Control reaches end of non-void function
    
    * Use a MoveItErrorCode cast
    
    * More efficient callAdapter()
    
    * More MoveItErrorCode
    
    * CI fixup attempt

--- moveit_ros/planning/planning_request_adapter_plugins/src/fix_workspace_bounds.cpp
+++ moveit_ros/planning/planning_request_adapter_plugins/src/fix_workspace_bounds.cpp
@@ -62,9 +62,11 @@ public:
     return "Fix Workspace Bounds";
   }
 
-  bool adaptAndPlan(const PlannerFn& planner, const planning_scene::PlanningSceneConstPtr& planning_scene,
-                    const planning_interface::MotionPlanRequest& req, planning_interface::MotionPlanResponse& res,
-                    std::vector<std::size_t>& /*added_path_index*/) const override
+  moveit::core::MoveItErrorCode adaptAndPlan(const PlannerFn& planner,
+                                             const planning_scene::PlanningSceneConstPtr& planning_scene,
+                                             const planning_interface::MotionPlanRequest& req,
+                                             planning_interface::MotionPlanResponse& res,
+                                             std::vector<std::size_t>& /*added_path_index*/) const override
   {
     RCLCPP_DEBUG(LOGGER, "Running '%s'", getDescription().c_str());
     const moveit_msgs::msg::WorkspaceParameters& wparams = req.workspace_parameters;
@@ -79,8 +81,8 @@ public:
       default_wp.max_corner.x = default_wp.max_corner.y = default_wp.max_corner.z = workspace_extent_;
       return planner(planning_scene, req2, res);
     }
-    else
-      return planner(planning_scene, req, res);
+
+    return planner(planning_scene, req, res);
   }
 
 private:
