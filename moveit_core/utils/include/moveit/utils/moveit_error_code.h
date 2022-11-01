180621ecfbe6a9b49d1d8aaa01d978eaaa536cec
AndyZe <zelenak@picknik.ai>
2022-11-01 10:01:07 -0500

    Add default case

--- moveit_core/utils/include/moveit/utils/moveit_error_code.h
+++ moveit_core/utils/include/moveit/utils/moveit_error_code.h
@@ -142,6 +142,8 @@ inline std::string error_code_to_string(MoveItErrorCode error_code)
       return std::string("ABORT");
     case moveit::core::MoveItErrorCode::NO_IK_SOLUTION:
       return std::string("NO_IK_SOLUTION");
+    default:
+      return std::string("Unrecognized MoveItErrorCode. This should never happen!");
   }
 }
 
