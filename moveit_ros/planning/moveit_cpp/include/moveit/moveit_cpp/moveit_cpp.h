83039e468654d8388838312188669308f2589da0
Sebastian Jahr <sebastian.jahr@tuta.io>
2022-11-07 16:47:56 +0100

    Update moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/moveit_cpp.h
    Co-authored-by: Robert Haschke <rhaschke@users.noreply.github.com>

--- moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/moveit_cpp.h
+++ moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/moveit_cpp.h
@@ -172,7 +172,7 @@ public:
                                                      const robot_trajectory::RobotTrajectoryPtr& robot_trajectory,
                                                      bool blocking = true);
 
-  /** \brief Utility to terminate a planning pipelines */
+  /** \brief Utility to terminate the given planning pipeline */
   bool terminatePlanningPipeline(const std::string& pipeline_name);
 
 private:
