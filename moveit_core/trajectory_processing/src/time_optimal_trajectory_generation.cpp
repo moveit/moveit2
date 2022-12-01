bf0496f09799c26b0b07e9858ebefac7ad5398dc
AndyZe <andyz@utexas.edu>
2022-12-01 14:29:59 -0600

    Add a version of TOTG computeTimeStamps() for a fixed num waypoints (#1771)
    * Add a version of computeTimeStamps() to yield a fixed num. waypoints
    
    * Add unit test
    
    * Prevent an ambiguous function signature
    
    * Remove debugging stuff
    
    * Can't have fewer than 2 waypoints
    
    * Warning about sparse waypoint spacing
    
    * Doxygen comments
    
    * Clarify about changing the shape of the path
    
    * Better comment
    
    Co-authored-by: Sebastian Jahr <sebastian.jahr@tuta.io>
    
    Co-authored-by: Sebastian Jahr <sebastian.jahr@tuta.io>

--- moveit_core/trajectory_processing/src/time_optimal_trajectory_generation.cpp
+++ moveit_core/trajectory_processing/src/time_optimal_trajectory_generation.cpp
@@ -1057,6 +1057,30 @@ bool TimeOptimalTrajectoryGeneration::computeTimeStamps(
   return doTimeParameterizationCalculations(trajectory, max_velocity, max_acceleration);
 }
 
+bool TimeOptimalTrajectoryGeneration::computeTimeStamps(const size_t num_waypoints,
+                                                        robot_trajectory::RobotTrajectory& trajectory,
+                                                        const double max_velocity_scaling_factor,
+                                                        const double max_acceleration_scaling_factor)
+{
+  // The algorithm is:
+  // 1. Run TOTG once to figure out the optimal trajectory duration
+  // 2. Calculate the timestep to get the desired num_waypoints:
+  //      new_delta_t = duration/(n-1)     // subtract one for the initial waypoint
+  // 3. Run TOTG again with the new timestep. This should give the exact num_waypoints you want
+
+  if (num_waypoints < 2)
+  {
+    RCLCPP_ERROR(LOGGER, "computeTimeStamps() requires num_waypoints > 1");
+    return false;
+  }
+
+  computeTimeStamps(trajectory, max_velocity_scaling_factor, max_acceleration_scaling_factor);
+  double optimal_duration = trajectory.getDuration();
+  resample_dt_ = optimal_duration / (num_waypoints - 1);
+  computeTimeStamps(trajectory, max_velocity_scaling_factor, max_acceleration_scaling_factor);
+  return true;
+}
+
 bool TimeOptimalTrajectoryGeneration::doTimeParameterizationCalculations(robot_trajectory::RobotTrajectory& trajectory,
                                                                          const Eigen::VectorXd& max_velocity,
                                                                          const Eigen::VectorXd& max_acceleration) const
