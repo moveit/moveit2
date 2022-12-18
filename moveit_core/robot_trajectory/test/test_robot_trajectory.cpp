0472c37dc719f72fceaa92248215fffdb86d89b0
AndyZe <andyz@utexas.edu>
2022-12-17 21:19:49 -0600

    Don't add to the timestep, rather overwrite it

--- moveit_core/robot_trajectory/test/test_robot_trajectory.cpp
+++ moveit_core/robot_trajectory/test/test_robot_trajectory.cpp
@@ -204,20 +204,21 @@ TEST_F(RobotTrajectoryTestFixture, Append)
 {
   robot_trajectory::RobotTrajectoryPtr initial_trajectory;
   initTestTrajectory(initial_trajectory);
+  EXPECT_EQ(initial_trajectory->getWayPointCount(), size_t(5));
 
   // Append to the first
   robot_trajectory::RobotTrajectoryPtr traj2;
   initTestTrajectory(traj2);
+  EXPECT_EQ(traj2->getWayPointCount(), size_t(5));
 
   // After append() we should have 10 waypoints, all with 0.1s duration
-  const double EXPECTED_DURATION = 0.1;
-  initial_trajectory->append(*traj2, 0.1, 0, 4);
+  const double expected_duration = 0.1;
+  initial_trajectory->append(*traj2, expected_duration, 0, 5);
+  EXPECT_EQ(initial_trajectory->getWayPointCount(), size_t(10));
 
-  EXPECT_EQ(traj2->getWayPointCount(), 5);
-  EXPECT_EQ(initial_trajectory->getWayPointCount(), 10);
-  EXPECT_EQ(initial_trajectory->getWayPointDurationFromPrevious(4), EXPECTED_DURATION);
-  EXPECT_EQ(initial_trajectory->getWayPointDurationFromPrevious(5), EXPECTED_DURATION);
-  EXPECT_EQ(initial_trajectory->getWayPointDurationFromPrevious(6), EXPECTED_DURATION);
+  EXPECT_EQ(initial_trajectory->getWayPointDurationFromPrevious(4), expected_duration);
+  EXPECT_EQ(initial_trajectory->getWayPointDurationFromPrevious(5), expected_duration);
+  EXPECT_EQ(initial_trajectory->getWayPointDurationFromPrevious(6), expected_duration);
 }
 
 TEST_F(RobotTrajectoryTestFixture, RobotTrajectoryShallowCopy)
