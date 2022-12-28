2838e89a64014dbcc81a27af54e4552c09c275d5
AndyZe <andyz@utexas.edu>
2022-12-28 11:05:51 -0600

    Do not allow traj execution from PlanningComponent

--- moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/planning_component.h
+++ moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/planning_component.h
@@ -200,10 +200,6 @@ public:
        const SolutionCallbackFunction& solution_selection_callback = &getShortestSolution,
        StoppingCriterionFunction stopping_criterion_callback = nullptr);
 
-  /** \brief Execute the latest computed solution trajectory computed by plan(). By default this function terminates
-   * after the execution is complete. The execution can be run in background by setting blocking to false. */
-  bool execute(bool blocking = true);
-
   /** \brief Return the last plan solution*/
   const planning_interface::MotionPlanResponse& getLastMotionPlanResponse();
 
