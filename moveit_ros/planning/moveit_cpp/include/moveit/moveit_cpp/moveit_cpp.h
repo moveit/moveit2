ef777e3fd294b9aa2f23becc91f4113b287adfb6
AndyZe <andyz@utexas.edu>
2023-01-09 08:53:55 -0600

    Add optional list of controllers

--- moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/moveit_cpp.h
+++ moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/moveit_cpp.h
@@ -164,9 +164,18 @@ public:
 
   /** \brief Execute a trajectory on the planning group specified by group_name using the trajectory execution manager.
    * If blocking is set to false, the execution is run in background and the function returns immediately. */
-  moveit_controller_manager::ExecutionStatus execute(const std::string& group_name,
-                                                     const robot_trajectory::RobotTrajectoryPtr& robot_trajectory,
-                                                     bool blocking = true);
+
+  /** \brief Execute a trajectory on the planning group specified by group_name using the trajectory execution manager.
+   *  \param [in] group_name MoveIt group to execute for.
+   *  \param [in] robot_trajectory Contains trajectory info as well as metadata such as a RobotModel.
+   *  \param [in] blocking If blocking, wait for the trajectory to execute before continuing. Defaults to `true`.
+   *  \param [in] controllers An optional list of ros2_controllers to execute with. If none, MoveIt will attempt to find
+   * a controller. The exact behavior of finding a controller depends on which MoveItControllerManager plugin is active.
+   * \return moveit_controller_manager::ExecutionStatus::SUCCEEDED if successful
+   */
+  moveit_controller_manager::ExecutionStatus
+  execute(const std::string& group_name, const robot_trajectory::RobotTrajectoryPtr& robot_trajectory,
+          bool blocking = true, const std::vector<std::string>& controllers = std::vector<std::string>(1, ""));
 
   /** \brief Utility to terminate the given planning pipeline */
   bool terminatePlanningPipeline(const std::string& pipeline_name);
