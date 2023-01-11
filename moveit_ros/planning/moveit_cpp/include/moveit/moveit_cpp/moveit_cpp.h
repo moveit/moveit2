17e2757bb22e49cecfe57bd67c238d4a7496f6c7
AndyZe <andyz@utexas.edu>
2023-01-11 11:11:32 -0600

    Rebase

--- moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/moveit_cpp.h
+++ moveit_ros/planning/moveit_cpp/include/moveit/moveit_cpp/moveit_cpp.h
@@ -173,10 +173,11 @@ public:
   [[deprecated(
       "MoveItCpp::execute() no longer requires a group_name parameter")]] moveit_controller_manager::ExecutionStatus
   execute(const std::string& group_name, const robot_trajectory::RobotTrajectoryPtr& robot_trajectory,
-          bool blocking = true);
+          bool blocking = true, const std::vector<std::string>& controllers = std::vector<std::string>());
 
-  moveit_controller_manager::ExecutionStatus execute(const robot_trajectory::RobotTrajectoryPtr& robot_trajectory,
-                                                     bool blocking = true);
+  moveit_controller_manager::ExecutionStatus
+  execute(const robot_trajectory::RobotTrajectoryPtr& robot_trajectory, bool blocking = true,
+          const std::vector<std::string>& controllers = std::vector<std::string>());
 
   /** \brief Utility to terminate the given planning pipeline */
   bool terminatePlanningPipeline(const std::string& pipeline_name);
