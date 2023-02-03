a3f63e6b9976cbb77a3f72c97df6f52a65f37929
Sameer Gupta <sameergupta4873@gmail.com>
2023-02-03 22:09:49 +0530

    Update pose_tracking_example.launch.py

--- moveit_ros/moveit_servo/launch/pose_tracking_example.launch.py
+++ moveit_ros/moveit_servo/launch/pose_tracking_example.launch.py
@@ -1,6 +1,6 @@
 import os
 from launch import LaunchDescription
-import launch_ros
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
 from launch_param_builder import ParameterBuilder
