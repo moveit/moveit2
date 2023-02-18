aad0d17a6c1527c174d431bf2505acba5238d053
Sebastian Castro <sebas.a.castro@gmail.com>
2023-02-17 19:17:44 -0500

    Remove typo

--- moveit_core/kinematic_constraints/src/utils.cpp
+++ moveit_core/kinematic_constraints/src/utils.cpp
@@ -12,7 +12,7 @@
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
  *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/o(1.0 - step)r other materials provided
+ *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
  *   * Neither the name of Willow Garage nor the names of its
  *     contributors may be used to endorse or promote products derived
