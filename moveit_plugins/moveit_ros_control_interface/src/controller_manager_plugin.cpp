40bf3a13ea218b4381bde6ce022ec309048a90ee
Erik Holum <erik.holum@picknik.ai>
2023-03-02 09:15:16 -0500

    Pre-commit fixes

--- moveit_plugins/moveit_ros_control_interface/src/controller_manager_plugin.cpp
+++ moveit_plugins/moveit_ros_control_interface/src/controller_manager_plugin.cpp
@@ -135,7 +135,7 @@ class Ros2ControlManager : public moveit_controller_manager::MoveItControllerMan
     if (!force && ((node_->now() - controllers_stamp_) < CONTROLLER_INFORMATION_VALIDITY_AGE))
     {
       RCLCPP_WARN_STREAM(LOGGER, "Controller information from " << list_controllers_service_->get_service_name()
-                                                               << " is out of date, skipping discovery");
+                                                                << " is out of date, skipping discovery");
       return;
     }
 
