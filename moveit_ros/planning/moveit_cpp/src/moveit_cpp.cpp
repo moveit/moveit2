7ac12dfb8c36974f37b2c172157c9493d791cf9f
Adarsh Singh <adarshsingh1093@gmail.com>
2023-03-07 02:59:41 +0530

    Hope this works

--- moveit_ros/planning/moveit_cpp/src/moveit_cpp.cpp
+++ moveit_ros/planning/moveit_cpp/src/moveit_cpp.cpp
@@ -46,10 +46,6 @@ namespace moveit_cpp
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("moveit.ros_planning_interface.moveit_cpp");
 constexpr char PLANNING_PLUGIN_PARAM[] = "planning_plugin";
 
-MoveItCpp::MoveItCpp(const rclcpp::Node::SharedPtr& node) : MoveItCpp(node, Options(node))
-{
-}
-
 MoveItCpp::MoveItCpp(const rclcpp::Node::SharedPtr& node, const Options& options) : node_(node)
 {
   // Configure planning scene monitor
