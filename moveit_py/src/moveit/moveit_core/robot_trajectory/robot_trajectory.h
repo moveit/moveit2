20c7e94af93294e2ca96b6077b13add47a595850
Jens Vanhooydonck <jensvanhooydonck@gmail.com>
2023-03-25 15:59:05 +0000

    Formatting of header files

--- moveit_py/src/moveit/moveit_core/robot_trajectory/robot_trajectory.h
+++ moveit_py/src/moveit/moveit_core/robot_trajectory/robot_trajectory.h
@@ -53,8 +53,7 @@ get_robot_trajectory_msg(const robot_trajectory::RobotTrajectoryConstPtr& robot_
                          const std::vector<std::string>& joint_filter);
 robot_trajectory::RobotTrajectory
 set_robot_trajectory_msg(const std::shared_ptr<robot_trajectory::RobotTrajectory>& robot_trajectory,
-                        const moveit::core::RobotState& robot_state,
-                        const moveit_msgs::msg::RobotTrajectory& msg);
+                         const moveit::core::RobotState& robot_state, const moveit_msgs::msg::RobotTrajectory& msg);
 
 void init_robot_trajectory(py::module& m);
 }  // namespace bind_robot_trajectory
