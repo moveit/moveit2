237d178b9c9ee115f9735b7f8ee3597fa8bfde20
ibrahiminfinite <ibrahimjkd@gmail.com>
2023-04-08 17:40:29 +0530

    Make servo_node partially use ParamListener

--- moveit_ros/moveit_servo/src/servo_node.cpp
+++ moveit_ros/moveit_servo/src/servo_node.cpp
@@ -79,10 +79,10 @@ ServoNode::ServoNode(const rclcpp::NodeOptions& options)
 
   // Get the servo parameters
   auto param_listener = std::make_shared<servo::ParamListener>(node_);
-  auto sparams = param_listener->get_params();
-  RCLCPP_INFO(LOGGER, "Got servo param : %f", sparams.rotational_scale);
-  auto servo_parameters = moveit_servo::ServoParameters::makeServoParameters(node_);
-  if (servo_parameters == nullptr)
+  auto servo_parameters = param_listener->get_params();
+  RCLCPP_INFO(LOGGER, "Got servo lib param : %f", servo_parameters.rotational_scale);
+  auto sparams = moveit_servo::ServoParameters::makeServoParameters(node_);
+  if (sparams == nullptr)
   {
     RCLCPP_ERROR(LOGGER, "Failed to load the servo parameters");
     throw std::runtime_error("Failed to load the servo parameters");
@@ -91,8 +91,8 @@ ServoNode::ServoNode(const rclcpp::NodeOptions& options)
   // Set up planning_scene_monitor
   planning_scene_monitor_ = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>(
       node_, robot_description_name, "planning_scene_monitor");
-  planning_scene_monitor_->startStateMonitor(servo_parameters->joint_topic);
-  planning_scene_monitor_->startSceneMonitor(servo_parameters->monitored_planning_scene_topic);
+  planning_scene_monitor_->startStateMonitor(servo_parameters.joint_topic);
+  planning_scene_monitor_->startSceneMonitor(servo_parameters.monitored_planning_scene_topic);
   planning_scene_monitor_->setPlanningScenePublishingFrequency(25);
   planning_scene_monitor_->getStateMonitor()->enableCopyDynamics(true);
   planning_scene_monitor_->startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE,
@@ -101,7 +101,7 @@ ServoNode::ServoNode(const rclcpp::NodeOptions& options)
   // If the planning scene monitor in servo is the primary one we provide /get_planning_scene service so RViz displays
   // or secondary planning scene monitors can fetch the scene, otherwise we request the planning scene from the
   // primary planning scene monitor (e.g. move_group)
-  if (servo_parameters->is_primary_planning_scene_monitor)
+  if (servo_parameters.is_primary_planning_scene_monitor)
   {
     planning_scene_monitor_->providePlanningSceneService();
   }
@@ -111,7 +111,7 @@ ServoNode::ServoNode(const rclcpp::NodeOptions& options)
   }
 
   // Create Servo
-  servo_ = std::make_unique<moveit_servo::Servo>(node_, servo_parameters, planning_scene_monitor_);
+  servo_ = std::make_unique<moveit_servo::Servo>(node_, sparams, planning_scene_monitor_, param_listener);
 }
 
 void ServoNode::startCB(const std::shared_ptr<std_srvs::srv::Trigger::Request>& /* unused */,
