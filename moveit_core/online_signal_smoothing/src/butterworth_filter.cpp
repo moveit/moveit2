689a2804ae4535a6ba4e1435b3b61415081c8f19
AndyZe <andyz@utexas.edu>
2023-04-11 15:25:26 -0500

    Member variable naming

--- moveit_core/online_signal_smoothing/src/butterworth_filter.cpp
+++ moveit_core/online_signal_smoothing/src/butterworth_filter.cpp
@@ -100,8 +100,8 @@ bool ButterworthFilterPlugin::initialize(rclcpp::Node::SharedPtr node, moveit::c
 {
   node_ = node;
   num_joints_ = num_joints;
-  auto param_listener_ = std::make_unique<online_signal_smoothing::ParamListener>(node_);
-  auto filter_coeff = param_listener_->get_params().butterworth_filter_coeff;
+  auto param_listener = std::make_unique<online_signal_smoothing::ParamListener>(node_);
+  auto filter_coeff = param_listener->get_params().butterworth_filter_coeff;
 
   for (std::size_t i = 0; i < num_joints_; ++i)
   {
