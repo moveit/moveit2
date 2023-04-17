b2089e489fd38186205acc3b20b50f8417123681
Nils-ChristianIseke <nilsmailiseke@gmail.com>
2023-04-17 11:21:36 +0000

    Removed const from return type const float.

--- moveit_ros/perception/mesh_filter/include/moveit/mesh_filter/gl_renderer.h
+++ moveit_ros/perception/mesh_filter/include/moveit/mesh_filter/gl_renderer.h
@@ -150,14 +150,14 @@ public:
    * \author Suat Gedikli (gedikli@willowgarage.com)
    * \return distance of near clipping plane in meters
    */
-  const float getNearClippingDistance() const;
+  float getNearClippingDistance() const;
 
   /**
    * \brief returns the distance of the far clipping plane in meters
    * \author Suat Gedikli (gedikli@willowgarage.com)
    * \return distance of the far clipping plane in meters
    */
-  const float getFarClippingDistance() const;
+  float getFarClippingDistance() const;
 
   /**
    * \brief returns the width of the frame buffer objectsin pixels
