b807db8999b190d9fdacfd0239c31ea3a06d85fd
Sebastian Jahr <sebastian.jahr@picknik.ai>
2023-04-26 15:03:23 +0200

    Add documentation and more cleanups

--- moveit_core/planning_request_adapter/src/planning_request_adapter.cpp
+++ moveit_core/planning_request_adapter/src/planning_request_adapter.cpp
@@ -107,6 +107,11 @@ bool PlanningRequestAdapter::adaptAndPlan(const planning_interface::PlannerManag
   return adaptAndPlan(planner, planning_scene, req, res, empty_added_path_index);
 }
 
+void PlanningRequestAdapterChain::addAdapter(const PlanningRequestAdapterConstPtr& adapter)
+{
+  adapters_.push_back(adapter);
+}
+
 bool PlanningRequestAdapterChain::adaptAndPlan(const planning_interface::PlannerManagerPtr& planner,
                                                const planning_scene::PlanningSceneConstPtr& planning_scene,
                                                const planning_interface::MotionPlanRequest& req,
@@ -140,7 +145,7 @@ bool PlanningRequestAdapterChain::adaptAndPlan(const planning_interface::Planner
     return callPlannerInterfaceSolve(planner, scene, req, res);
   };
 
-  for (int i = adapters_.size() - 1; i >= 0; --i)
+  for (std::size_t i = adapters_.size() - 1; i >= 0; --i)
   {
     fn = [&adapter = *adapters_[i], fn, &added_path_index = added_path_index_each[i]](
              const planning_scene::PlanningSceneConstPtr& scene, const planning_interface::MotionPlanRequest& req,
