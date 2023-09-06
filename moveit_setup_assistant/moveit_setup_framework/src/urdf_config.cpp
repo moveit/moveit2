moveit/moveit2

commit 06e5a0b29107432c8a42e1986d356193383347b8

Add URDF Loader Exceptions and Fix Infinite While-Loop when URDF file isn't in a ROS Package (#2032)
* Fixed infinite while loop in utilities.cpp and added some exception handling to start screen widget

* Fix trailing whitespace, fix getSharePath exception catch on empty request

* Fix clang tidy suggestion and error message updates based on pr comments

moveit_setup_assistant/moveit_setup_framework/src/urdf_config.cpp
@@ -98,18 +98,15 @@ void URDFConfig::setPackageName()
     urdf_pkg_name_ = "";
     urdf_pkg_relative_path_ = urdf_path_;  // just the absolute path
   }
-  else
-  {
-    // Check that ROS can find the package
-    const std::filesystem::path robot_desc_pkg_path = getSharePath(urdf_pkg_name_);
+  // Check that ROS can find the package
+  const std::filesystem::path robot_desc_pkg_path = getSharePath(urdf_pkg_name_);
 
-    if (robot_desc_pkg_path.empty())
-    {
-      RCLCPP_WARN(*logger_,
-                  "Package Not Found In ROS Workspace. ROS was unable to find the package name '%s'"
-                  " within the ROS workspace. This may cause issues later.",
-                  urdf_pkg_name_.c_str());
-    }
+  if (robot_desc_pkg_path.empty())
+  {
+    RCLCPP_WARN(*logger_,
+                "Package Not Found In ROS Workspace. ROS was unable to find the package name '%s'"
+                " within the ROS workspace. This may cause issues later.",
+                urdf_pkg_name_.c_str());
   }
 }
 
@@ -132,6 +129,11 @@ void URDFConfig::load()
   if (!rdf_loader::RDFLoader::loadXmlFileToString(urdf_string_, urdf_path_, xacro_args_vec_))
   {
     throw std::runtime_error("URDF/COLLADA file not found: " + urdf_path_.string());
+  }
+
+  if (urdf_pkg_name_.empty())
+  {
+    throw std::runtime_error("URDF/COLLADA package not found for file path: " + urdf_path_.string());
   }
 
   if (urdf_string_.empty() && rdf_loader::RDFLoader::isXacroFile(urdf_path_))

moveit_setup_assistant/moveit_setup_core_plugins/src/start_screen_widget.cpp
@@ -305,8 +305,16 @@ void StartScreenWidget::onPackagePathChanged(const QString& /*path*/)
 
 void StartScreenWidget::onUrdfPathChanged(const QString& path)
 {
-  setup_step_.loadURDFFile(path.toStdString(), urdf_file_->getArgs().toStdString());
-  urdf_file_->setArgsEnabled(setup_step_.isXacroFile());
+  try
+  {
+    setup_step_.loadURDFFile(path.toStdString(), urdf_file_->getArgs().toStdString());
+    urdf_file_->setArgsEnabled(setup_step_.isXacroFile());
+  }
+  catch (const std::runtime_error& e)
+  {
+    RCLCPP_ERROR(setup_step_.getLogger(), "Error Loading URDF Path. %s", e.what());
+    QMessageBox::warning(this, "Error Loading URDF Path", QString(e.what()));
+  }
 }
 
 bool StartScreenWidget::loadPackageSettings(bool show_warnings)
@@ -343,8 +351,8 @@ bool StartScreenWidget::loadExistingFiles()
   }
   catch (const std::runtime_error& e)
   {
+    RCLCPP_ERROR(setup_step_.getLogger(), "Error Loading SRDF. %s", e.what());
     QMessageBox::warning(this, "Error Loading SRDF", QString(e.what()));
-    RCLCPP_ERROR(setup_step_.getLogger(), "%s", e.what());
     return false;
   }
 

moveit_setup_assistant/moveit_setup_framework/include/moveit_setup_framework/utilities.hpp
@@ -36,6 +36,7 @@
 
 #pragma once
 
+#include <ament_index_cpp/get_package_prefix.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <filesystem>
 #include <string>
@@ -49,7 +50,14 @@ namespace moveit_setup
  */
 inline std::filesystem::path getSharePath(const std::string& package_name)
 {
-  return std::filesystem::path(ament_index_cpp::get_package_share_directory(package_name));
+  try
+  {
+    return std::filesystem::path(ament_index_cpp::get_package_share_directory(package_name));
+  }
+  catch (const std::runtime_error& e)
+  {
+    return std::filesystem::path();
+  }
 }
 
 /**

moveit_setup_assistant/moveit_setup_framework/src/utilities.cpp
@@ -53,7 +53,8 @@ bool extractPackageNameFromPath(const std::filesystem::path& path, std::string& 
   }
 
   // truncate path step by step and check if it contains a package.xml
-  while (!sub_path.empty())
+  // This runs until the path is either empty "" or at the root "/" or "C:\\"
+  while (!sub_path.empty() && sub_path != sub_path.root_path())
   {
     if (std::filesystem::is_regular_file(sub_path / "package.xml"))
     {

commit 01d87fb258a279882b03fbe65a931350bd49bd26

Fix collisions_updater CLI if no package is used
The exceptions introduced with https://github.com/ros-planning/moveit2/pull/2032
prevented from running the collisions updater CLI without a ROS package.
This fix makes ROS packages optional again, allowing to use the CLI with absolute
paths only.

moveit_setup_assistant/moveit_setup_framework/src/urdf_config.cpp
@@ -92,32 +92,45 @@ void URDFConfig::loadFromPath(const std::filesystem::path& urdf_file_path, const
 
 void URDFConfig::setPackageName()
 {
-  bool package_found = extractPackageNameFromPath(urdf_path_, urdf_pkg_name_, urdf_pkg_relative_path_);
-  if (!package_found)
-  {
-    urdf_pkg_name_ = "";
-    urdf_pkg_relative_path_ = urdf_path_;  // just the absolute path
-  }
-  // Check that ROS can find the package
-  const std::filesystem::path robot_desc_pkg_path = getSharePath(urdf_pkg_name_);
+  // Reset to defaults: no package name, relative path is set to absolute path
+  urdf_pkg_name_ = "";
+  urdf_pkg_relative_path_ = urdf_path_;
 
-  if (robot_desc_pkg_path.empty())
+  std::string pkg_name;
+  std::filesystem::path relative_path;
+  if (extractPackageNameFromPath(urdf_path_, pkg_name, relative_path))
   {
-    RCLCPP_WARN(*logger_,
-                "Package Not Found In ROS Workspace. ROS was unable to find the package name '%s'"
-                " within the ROS workspace. This may cause issues later.",
-                urdf_pkg_name_.c_str());
+    // Check that ROS can find the package, update members accordingly
+    const std::filesystem::path robot_desc_pkg_path = getSharePath(pkg_name);
+    if (!robot_desc_pkg_path.empty())
+    {
+      urdf_pkg_name_ = pkg_name;
+      urdf_pkg_relative_path_ = relative_path;
+    }
+    else
+    {
+      RCLCPP_WARN(*logger_,
+                  "Package Not Found In ROS Workspace. ROS was unable to find the package name '%s'"
+                  " within the ROS workspace. This may cause issues later.",
+                  pkg_name.c_str());
+    }
   }
 }
 
 void URDFConfig::loadFromPackage(const std::filesystem::path& package_name, const std::filesystem::path& relative_path,
                                  const std::string& xacro_args)
 {
+  const std::filesystem::path package_path = getSharePath(package_name);
+  if (package_path.empty())
+  {
+    throw std::runtime_error("URDF/COLLADA package not found: ''" + package_name.string());
+  }
+
   urdf_pkg_name_ = package_name;
   urdf_pkg_relative_path_ = relative_path;
   xacro_args_ = xacro_args;
 
-  urdf_path_ = getSharePath(urdf_pkg_name_) / relative_path;
+  urdf_path_ = package_path / relative_path;
   load();
 }
 
@@ -129,11 +142,6 @@ void URDFConfig::load()
   if (!rdf_loader::RDFLoader::loadXmlFileToString(urdf_string_, urdf_path_, xacro_args_vec_))
   {
     throw std::runtime_error("URDF/COLLADA file not found: " + urdf_path_.string());
-  }
-
-  if (urdf_pkg_name_.empty())
-  {
-    throw std::runtime_error("URDF/COLLADA package not found for file path: " + urdf_path_.string());
   }
 
   if (urdf_string_.empty() && rdf_loader::RDFLoader::isXacroFile(urdf_path_))