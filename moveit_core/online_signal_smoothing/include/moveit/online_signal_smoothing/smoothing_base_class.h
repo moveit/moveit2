e180630ef9d15c5839ebebbc8e77617aba7abb0a
ibrahiminfinite <ibrahimjkd@gmail.com>
2023-10-03 18:47:16 +0530

    Fix dependencies

--- moveit_core/online_signal_smoothing/include/moveit/online_signal_smoothing/smoothing_base_class.h
+++ moveit_core/online_signal_smoothing/include/moveit/online_signal_smoothing/smoothing_base_class.h
@@ -38,12 +38,12 @@
 
 #pragma once
 
-#include <optional>
-
 #include <rclcpp/rclcpp.hpp>
-#include <tf2_eigen/tf2_eigen.hpp>
+
 #include <moveit/macros/class_forward.h>
 #include <moveit_smoothing_base_export.h>
+
+#include <tf2_eigen/tf2_eigen.hpp>
 
 namespace moveit
 {
