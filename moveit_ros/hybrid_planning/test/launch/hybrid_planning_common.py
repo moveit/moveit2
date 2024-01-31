c19b1eb182f65aea02869333fd93d1dd57b4dea6
Sebastian Jahr <sebastian.jahr@picknik.ai>
2024-01-31 16:54:32 +0100

    Clean-up hybrid planning package (#2603)

--- moveit_ros/hybrid_planning/test/launch/hybrid_planning_common.py
+++ moveit_ros/hybrid_planning/test/launch/hybrid_planning_common.py
@@ -1,26 +1,11 @@
 # Return a list of nodes we commonly launch for the demo. Nice for testing use.
 import os
-import xacro
 import yaml
 
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import ExecuteProcess
-from launch_ros.actions import Node
 from launch_ros.descriptions import ComposableNode
 from launch_ros.actions import ComposableNodeContainer
-
-
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, "r") as file:
-            return file.read()
-    except (
-        EnvironmentError
-    ):  # parent of IOError, OSError *and* WindowsError where available
-        return None
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
 def load_yaml(package_name, file_path):
@@ -36,71 +21,21 @@ def load_yaml(package_name, file_path):
         return None
 
 
-def get_robot_description():
-    robot_description_config = xacro.process_file(
-        os.path.join(
-            get_package_share_directory("moveit_resources_panda_moveit_config"),
-            "config",
-            "panda.urdf.xacro",
-        )
-    )
-    robot_description = {"robot_description": robot_description_config.toxml()}
-    return robot_description
-
-
-def get_robot_description_semantic():
-    robot_description_semantic_config = load_file(
-        "moveit_resources_panda_moveit_config", "config/panda.srdf"
-    )
-    robot_description_semantic = {
-        "robot_description_semantic": robot_description_semantic_config
-    }
-    return robot_description_semantic
-
-
 def generate_common_hybrid_launch_description():
-    robot_description = get_robot_description()
-
-    robot_description_semantic = get_robot_description_semantic()
-
-    kinematics_yaml = load_yaml(
-        "moveit_resources_panda_moveit_config", "config/kinematics.yaml"
+    # Load the moveit configuration
+    moveit_config = (
+        MoveItConfigsBuilder("moveit_resources_panda")
+        .robot_description(file_path="config/panda.urdf.xacro")
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .planning_pipelines("ompl", ["ompl"])
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .to_moveit_configs()
     )
 
-    # The global planner uses the typical OMPL parameters
-    planning_pipelines_config = {
-        "ompl": {
-            "planning_plugins": ["ompl_interface/OMPLPlanner"],
-            "request_adapters": [
-                "default_planning_request_adapters/ResolveConstraintFrames",
-                "default_planning_request_adapters/ValidateWorkspaceBounds",
-                "default_planning_request_adapters/CheckStartStateBounds",
-                "default_planning_request_adapters/CheckStartStateCollision",
-            ],
-            "response_adapters": [
-                "default_planning_response_adapters/AddTimeOptimalParameterization",
-                "default_planning_response_adapters/ValidateSolution",
-                "default_planning_response_adapters/DisplayMotionPath",
-            ],
-        }
-    }
-    ompl_planning_yaml = load_yaml(
-        "moveit_resources_panda_moveit_config", "config/ompl_planning.yaml"
-    )
-    planning_pipelines_config["ompl"].update(ompl_planning_yaml)
-
-    moveit_simple_controllers_yaml = load_yaml(
-        "moveit_resources_panda_moveit_config", "config/moveit_controllers.yaml"
-    )
-    moveit_controllers = {
-        "moveit_simple_controller_manager": moveit_simple_controllers_yaml,
-        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
-    }
-
+    # Hybrid planning parameter
     # Any parameters that are unique to your plugins go here
-    common_hybrid_planning_param = load_yaml(
-        "moveit_hybrid_planning", "config/common_hybrid_planning_params.yaml"
-    )
     global_planner_param = load_yaml(
         "moveit_hybrid_planning", "config/global_planner.yaml"
     )
@@ -123,13 +58,8 @@ def generate_common_hybrid_launch_description():
                 plugin="moveit::hybrid_planning::GlobalPlannerComponent",
                 name="global_planner",
                 parameters=[
-                    common_hybrid_planning_param,
                     global_planner_param,
-                    robot_description,
-                    robot_description_semantic,
-                    kinematics_yaml,
-                    planning_pipelines_config,
-                    moveit_controllers,
+                    moveit_config.to_dict(),
                 ],
             ),
             ComposableNode(
@@ -137,11 +67,8 @@ def generate_common_hybrid_launch_description():
                 plugin="moveit::hybrid_planning::LocalPlannerComponent",
                 name="local_planner",
                 parameters=[
-                    common_hybrid_planning_param,
                     local_planner_param,
-                    robot_description,
-                    robot_description_semantic,
-                    kinematics_yaml,
+                    moveit_config.to_dict(),
                 ],
             ),
             ComposableNode(
@@ -149,7 +76,6 @@ def generate_common_hybrid_launch_description():
                 plugin="moveit::hybrid_planning::HybridPlanningManager",
                 name="hybrid_planning_manager",
                 parameters=[
-                    common_hybrid_planning_param,
                     hybrid_planning_manager_param,
                 ],
             ),
@@ -157,82 +83,8 @@ def generate_common_hybrid_launch_description():
         output="screen",
     )
 
-    # RViz
-    rviz_config_file = (
-        get_package_share_directory("moveit_hybrid_planning")
-        + "/config/hybrid_planning_demo.rviz"
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        parameters=[robot_description, robot_description_semantic],
-    )
-
-    # Static TF
-    static_tf = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        name="static_transform_publisher",
-        output="log",
-        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "panda_link0"],
-    )
-
-    # Publish TF
-    robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="both",
-        parameters=[robot_description],
-    )
-
-    # ros2_control using FakeSystem as hardware
-    ros2_controllers_path = os.path.join(
-        get_package_share_directory("moveit_hybrid_planning"),
-        "config",
-        "demo_controller.yaml",
-    )
-    ros2_control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[ros2_controllers_path],
-        remappings=[
-            ("/controller_manager/robot_description", "/robot_description"),
-        ],
-        output="screen",
-    )
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
-    )
-
-    panda_joint_group_position_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "panda_joint_group_position_controller",
-            "-c",
-            "/controller_manager",
-        ],
-    )
-
     launched_nodes = [
         container,
-        static_tf,
-        rviz_node,
-        robot_state_publisher,
-        ros2_control_node,
-        joint_state_broadcaster_spawner,
-        panda_joint_group_position_controller_spawner,
     ]
 
     return launched_nodes
