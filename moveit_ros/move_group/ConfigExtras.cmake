91137f9beb587bf62d18cfd8355c6c111505a627
Tyler Weaver <maybe@tylerjw.dev>
2024-02-12 20:46:19 -0700

    cmake-format

--- moveit_ros/move_group/ConfigExtras.cmake
+++ moveit_ros/move_group/ConfigExtras.cmake
@@ -1,3 +1,10 @@
 # Extras module needed for dependencies to find boost components
 
-find_package(Boost REQUIRED system filesystem date_time program_options thread)
+find_package(
+  Boost
+  REQUIRED
+  system
+  filesystem
+  date_time
+  program_options
+  thread)
