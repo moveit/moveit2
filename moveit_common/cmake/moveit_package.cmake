80b54abaa65808893339a6c40b75ea43b38b6d96
Tyler Weaver <maybe@tylerjw.dev>
2024-02-12 21:05:27 -0700

    cmake_lint

--- moveit_common/cmake/moveit_package.cmake
+++ moveit_common/cmake/moveit_package.cmake
@@ -25,7 +25,8 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-macro(moveit_package)
+# Module for common settings in MoveIt packages.
+macro(MOVEIT_PACKAGE)
   # Set ${PROJECT_NAME}_VERSION
   find_package(ament_cmake REQUIRED)
   ament_package_xml()
