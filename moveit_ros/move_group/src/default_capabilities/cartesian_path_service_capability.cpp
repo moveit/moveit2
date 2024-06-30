4a7d8ad8a16be2d3fc3154502530a048c01ca5a1
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2024-06-30 19:31:54 +0200

    Replace deprecated functions

--- moveit_ros/move_group/src/default_capabilities/cartesian_path_service_capability.cpp
+++ moveit_ros/move_group/src/default_capabilities/cartesian_path_service_capability.cpp
@@ -185,7 +185,7 @@ bool MoveGroupCartesianPathService::computeService(
           std::vector<moveit::core::RobotStatePtr> traj;
           res->fraction = moveit::core::CartesianInterpolator::computeCartesianPath(
               &start_state, jmg, traj, start_state.getLinkModel(link_name), waypoints, global_frame,
-              moveit::core::MaxEEFStep(req->max_step), jump_threshold, constraint_fn);
+              moveit::core::MaxEEFStep(req->max_step), moveit::core::CartesianPrecision{}, constraint_fn);
           moveit::core::robotStateToRobotStateMsg(start_state, res->start_state);
 
           robot_trajectory::RobotTrajectory rt(context_->planning_scene_monitor_->getRobotModel(), req->group_name);
