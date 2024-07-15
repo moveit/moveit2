920dcb22ea61b47e501aa6e97b4263520d618218
Boone Adkins <boone.adkins@gmail.com>
2024-07-15 16:21:45 -0700

    Reverts capabilities default value to the empty string. Reverts PR-2696, which broke MoveIt launch files output by the setup assistant.

--- moveit_configs_utils/moveit_configs_utils/launches.py
+++ moveit_configs_utils/moveit_configs_utils/launches.py
@@ -200,7 +200,7 @@ def generate_move_group_launch(moveit_config):
     ld.add_action(
         DeclareLaunchArgument(
             "capabilities",
-            default_value=moveit_config.move_group_capabilities["capabilities"],
+            default_value="",
         )
     )
     # inhibit these default MoveGroup capabilities (space separated)
