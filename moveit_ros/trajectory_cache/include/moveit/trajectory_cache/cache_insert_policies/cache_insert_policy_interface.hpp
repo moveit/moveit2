0d64c97f37066c573ef71a797660a63826843642
methylDragon <methylDragon@google.com>
2024-08-03 22:58:54 -0700

    Add CartesianAlwaysInsertNeverPrunePolicy
    Signed-off-by: methylDragon <methylDragon@gmail.com>

--- moveit_ros/trajectory_cache/include/moveit/trajectory_cache/cache_insert_policies/cache_insert_policy_interface.hpp
+++ moveit_ros/trajectory_cache/include/moveit/trajectory_cache/cache_insert_policies/cache_insert_policy_interface.hpp
@@ -12,7 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* Author: methylDragon */
+/** @file
+ * @brief Abstract template class for injecting logic for determining when to prune and insert a
+ * cache entry, and what metadata to attach to the cache entry.
+ *
+ * @author methylDragon
+ */
 
 #pragma once
 
@@ -28,22 +33,23 @@ namespace trajectory_cache
  * @headerfile cache_insert_policy_interface.hpp
  * "moveit/trajectory_cache/cache_insert_policies/cache_insert_policy_interface.hpp"
  *
- * @brief Abstract class for injecting logic for determining when to prune and insert a cache entry, and what metadata
- * to attach to the cache entry.
+ * @brief Abstract class for injecting logic for determining when to prune and insert a cache entry,
+ * and what metadata to attach to the cache entry.
  *
  * @tparam KeyT. The object to extract features from which to key the cache. Typically the plan request.
  * @tparam ValueT. The object that the TrajectoryCache was passed to insert. Typically the plan response.
  * @tparam CacheEntryT. The object stored in the cache entry.
  *
- * Notably, implementations of this interface are used to determine all the logic leading up to, but not including, the
- * insert call itself.
+ * Notably, implementations of this interface are used to determine all the logic leading up to, but
+ * not including, the insert call itself.
  *
- * Additionally, the choice of which implementation to use will constraint the set of FeaturesInterface<FeatureSourceT>
- * implementations that can be used to fetch cache entries with, with a caveat mentioned later.
+ * Additionally, the choice of which implementation to use will constraint the set of
+ * FeaturesInterface<FeatureSourceT> implementations that can be used to fetch cache entries with,
+ * with a caveat mentioned later.
  *
- * Users may implement this interface to add additional cache insertion functionality beyond the ones provided by this
- * package (e.g., prioritizing minimum jerk, or minimal path length), and the set of metadata considered when inserting
- * the cache entry.
+ * Users may implement this interface to add additional cache insertion functionality beyond the
+ * ones provided by this package (e.g., prioritizing minimum jerk, or minimal path length), and the
+ * set of metadata considered when inserting the cache entry.
  *
  * @see TrajectoryCache
  * @see FeaturesInterface<FeatureSourceT>
@@ -53,26 +59,28 @@ namespace trajectory_cache
  * Each policy makes certain decisions about what pieces of metadata to attach to the cache
  * entry for later fetching.
  *
- * As such, an implementation will necessarily constrain the set of FeaturesInterface<FeatureSourceT> implementations
- * that can be used to fetch entries from the cache that were tagged by a policy.
+ * As such, an implementation will necessarily constrain the set of FeaturesInterface<FeatureSourceT>
+ * implementations that can be used to fetch entries from the cache that were tagged by a policy.
  *
- * There is no way to meaningfully express this coupling at compile time, so users must ensure that they read the
- * documentation of each implementation of the interface to determine what FeaturesInterface<FeatureSourceT>
- * implementations can be used with cache entries processed by the implementation.
+ * There is no way to meaningfully express this coupling at compile time, so users must ensure that
+ * they read the documentation of each implementation of the interface to determine what
+ * FeaturesInterface<FeatureSourceT> implementations can be used with cache entries processed by the
+ * implementation.
  *
  * Two mitigations may be applied to alleviate this:
- *   1. Implementations of this interface may expose helper methods that can be called at runtime that take in arbitrary
- *      configuration parameters to them initialize the features that are used to fetch cache entries at point of use.
+ *   1. Implementations of this interface may expose helper methods that can be called at runtime
+ *      that take in arbitrary configuration parameters to them initialize the features that are
+ *      used to fetch cache entries at point of use.
  *
- *   2. The TrajectoryCache class' insertion methods allow the bubbling down of additional user-specified features to
- *      be appended by the TrajectoryCache independent of a policy, which can be used to
- *      support features that are not explicitly supported by the policy.
+ *   2. The TrajectoryCache class' insertion methods allow the bubbling down of additional
+ *      user-specified features to be appended by the TrajectoryCache independent of a policy, which
+ *      can be used to support features that are not explicitly supported by the policy.
  *
- *      Care must be taken to ensure that there are no overlaps when doing so, though, because it could potentially
- *      result in duplicate metadata entries otherwise.
+ *      Care must be taken to ensure that there are no overlaps when doing so, though, because it
+ *      could potentially result in duplicate metadata entries otherwise.
  *
- * Consequently, a set of FeaturesInterface<FeatureSourceT> implementations can be used to fetch a cache entry if that
- * set is a subset of the union of:
+ * Consequently, a set of FeaturesInterface<FeatureSourceT> implementations can be used to fetch a
+ * cache entry if that set is a subset of the union of:
  *   - The `additional_features` passed to `TrajectoryCache`
  *   - The features used by a policy to append metadata to the cache entry
  *
@@ -80,23 +88,24 @@ namespace trajectory_cache
  * ^^^^^^^^^^^^^^^^^^^^^
  * Pruning is a two step process:
  *   1. Fetch all "matching" cache entries, as defined by the policy
- *   2. From the fetched "matching" entries, subject each to the `prunePredicate` method, again defined by the
- *      policy.
+ *   2. From the fetched "matching" entries, subject each to the `prunePredicate` method, again
+ *      defined by the policy.
  *
  * This allows a user to define a policy to match and prune on any arbitrary properties of
  * the cache entries and insertion candidate.
  *
  * Similarly, logic can be injected to determine when the insertion candidate should be inserted.
  *
- * NOTE: The TrajectoryCache class also has some top-level logic to preserve cache entries that would have been pruned.
+ * NOTE: The TrajectoryCache class also has some top-level logic to preserve cache entries that
+ * would have been pruned.
  *
  * Stateful Policies
  * ^^^^^^^^^^^^^^^^^
- * Finally, as there can be information that must be preserved across the multiple steps of the match-prune-insert
- * process, this interface assumes stateful operation within a single insert call.
+ * Finally, as there can be information that must be preserved across the multiple steps of the
+ * match-prune-insert process, this interface assumes stateful operation within a single insert call.
  *
- * For example, preserving information in state will be required to propagate aggregated or rollup information about the
- * entire set of matched cache entries to future calls.
+ * For example, preserving information in state will be required to propagate aggregated or rollup
+ * information about the entire set of matched cache entries to future calls.
  *
  * The TrajectoryCache will call `reset()` on the policy at the end of the insert call.
  *
@@ -125,8 +134,8 @@ public:
    * @param[in] coll. The cache database to fetch messages from.
    * @param[in] key. The object used to key the insertion candidate with.
    * @param[in] value. The object that the TrajectoryCache was passed to insert.
-   * @returns moveit::core::MoveItErrorCode::SUCCESS if the cache insert should proceed. Otherwise, will return a
-   different error code with the reason why it should not.
+   * @returns moveit::core::MoveItErrorCode::SUCCESS if the cache insert should proceed. Otherwise,
+   * will return a different error code with the reason why it should not.
    */
   virtual moveit::core::MoveItErrorCode
   checkCacheInsertInputs(const moveit::planning_interface::MoveGroupInterface& move_group,
@@ -135,9 +144,12 @@ public:
 
   /** @brief Fetches all "matching" cache entries for comparison for pruning.
    *
+   * This method should be assumed to only return the message metadata without the underlying
+   * message data.
+   *
    * The policy should also make the decision about how to sort them.
-   * The order in which cache entries are presented to the prunePredicate will be the order of cache entries returned by
-   * this function.
+   * The order in which cache entries are presented to the prunePredicate will be the order of cache
+   * entries returned by this function.
    *
    * @see CacheInsertPolicyInterface<KeyT, ValueT, CacheEntryT>#prunePredicate
    *
@@ -146,7 +158,7 @@ public:
    * @param[in] key. The object used to key the insertion candidate with.
    * @param[in] value. The object that the TrajectoryCache was passed to insert.
    * @param[in] exact_match_precision. Tolerance for float precision comparison for what counts as an exact match.
-   * @returns A vector of matching cache entries for use by the other methods.
+   * @returns A vector of only the metadata of matching cache entries for use by the other methods.
    */
   virtual std::vector<typename warehouse_ros::MessageWithMetadata<CacheEntryT>::ConstPtr>
   fetchMatchingEntries(const moveit::planning_interface::MoveGroupInterface& move_group,
@@ -155,8 +167,8 @@ public:
 
   /** @brief Returns whether a matched cache entry should be pruned.
    *
-   * NOTE: The TrajectoryCache class also has some top-level logic to preserve cache entries that would have been
-   * pruned.
+   * NOTE: The TrajectoryCache class also has some top-level logic to preserve cache entries that
+   * would have been pruned.
    *
    * @param[in] move_group. The manipulator move group, used to get its state.
    * @param[in] key. The object used to key the insertion candidate with.
@@ -170,8 +182,8 @@ public:
 
   /** @brief Returns whether the insertion candidate should be inserted into the cache.
    *
-   * NOTE: The TrajectoryCache class executes the insert, but this class informs it on whether the insert should happen
-   * or not.
+   * NOTE: The TrajectoryCache class executes the insert, but this class informs it on whether the
+   * insert should happen or not.
    *
    * @param[in] move_group. The manipulator move group, used to get its state.
    * @param[in] key. The object used to key the insertion candidate with.
@@ -189,8 +201,9 @@ public:
    * @param[in] move_group. The manipulator move group, used to get its state.
    * @param[in] key. The object used to key the insertion candidate with.
    * @param[in] value. The object that the TrajectoryCache was passed to insert.
-   * @returns moveit::core::MoveItErrorCode::SUCCESS if successfully appended. Otherwise, will return a different error
-   * code, in which case the metadata should not be reused, and the TrajectoryCache will abort the insert.
+   * @returns moveit::core::MoveItErrorCode::SUCCESS if successfully appended. Otherwise, will
+   * return a different error code, in which case the metadata should not be reused, and the
+   * TrajectoryCache will abort the insert.
    */
   virtual moveit::core::MoveItErrorCode
   appendInsertMetadata(warehouse_ros::Metadata& metadata,
--- moveit_ros/trajectory_cache/src/cache_insert_policies/cartesian_always_insert_never_prune_policy.cpp
+++ moveit_ros/trajectory_cache/src/cache_insert_policies/cartesian_always_insert_never_prune_policy.cpp
@@ -14,7 +14,7 @@
 
 /** @file
  * @brief Implementation of a cache insertion policy that always decides to insert and never decides
- * to prune.
+ * to prune for motion cartesian path requests.
  *
  * @see CacheInsertPolicyInterface<KeyT, ValueT, CacheEntryT>
  *
@@ -27,14 +27,11 @@
 
 #include <warehouse_ros/message_collection.h>
 #include <moveit/move_group_interface/move_group_interface.h>
-
 #include <moveit_msgs/msg/motion_plan_request.hpp>
-#include <moveit_msgs/srv/get_cartesian_path.hpp>
 
-#include <moveit/trajectory_cache/cache_insert_policies/always_insert_never_prune_policy.hpp>
 #include <moveit/trajectory_cache/features/constant_features.hpp>
 #include <moveit/trajectory_cache/features/get_cartesian_path_request_features.hpp>
-#include <moveit/trajectory_cache/features/motion_plan_request_features.hpp>
+#include <moveit/trajectory_cache/cache_insert_policies/cartesian_always_insert_never_prune_policy.hpp>
 #include <moveit/trajectory_cache/utils/utils.hpp>
 
 namespace moveit_ros
@@ -50,230 +47,19 @@ using ::warehouse_ros::Query;
 using ::moveit::core::MoveItErrorCode;
 using ::moveit::planning_interface::MoveGroupInterface;
 
-using ::moveit_msgs::msg::MotionPlanRequest;
 using ::moveit_msgs::msg::RobotTrajectory;
 using ::moveit_msgs::srv::GetCartesianPath;
-
 using ::moveit_ros::trajectory_cache::FeaturesInterface;
 
 namespace
 {
 
-static const std::string EXECUTION_TIME = "execution_time_s";
 static const std::string FRACTION = "fraction";
-static const std::string PLANNING_TIME = "planning_time_s";
+static const std::string EXECUTION_TIME = "execution_time_s";
 
 }  // namespace
 
-// =================================================================================================
-// AlwaysInsertNeverPrunePolicy.
-// =================================================================================================
-// moveit_msgs::msg::MotionPlanRequest <=> moveit::planning_interface::MoveGroupInterface::Plan
-
-AlwaysInsertNeverPrunePolicy::AlwaysInsertNeverPrunePolicy() : name_("AlwaysInsertNeverPrunePolicy")
-{
-}
-
-std::vector<std::unique_ptr<FeaturesInterface<MotionPlanRequest>>>
-AlwaysInsertNeverPrunePolicy::getSupportedFeatures(double start_tolerance, double goal_tolerance)
-{
-  std::vector<std::unique_ptr<FeaturesInterface<MotionPlanRequest>>> out;
-
-  // Start.
-  out.push_back(std::make_unique<WorkspaceFeatures>());
-  out.push_back(std::make_unique<StartStateJointStateFeatures>(start_tolerance));
-
-  // Goal.
-  out.push_back(std::make_unique<MaxSpeedAndAccelerationFeatures>());
-  out.push_back(std::make_unique<GoalConstraintsFeatures>(goal_tolerance));
-  out.push_back(std::make_unique<PathConstraintsFeatures>(goal_tolerance));
-  out.push_back(std::make_unique<TrajectoryConstraintsFeatures>(goal_tolerance));
-
-  return out;
-}
-
-std::string AlwaysInsertNeverPrunePolicy::getName() const
-{
-  return name_;
-}
-
-MoveItErrorCode AlwaysInsertNeverPrunePolicy::checkCacheInsertInputs(const MoveGroupInterface& move_group,
-                                                                     const MessageCollection<RobotTrajectory>& /*coll*/,
-                                                                     const MotionPlanRequest& key,
-                                                                     const MoveGroupInterface::Plan& value)
-{
-  std::string workspace_frame_id = getWorkspaceFrameId(move_group, key.workspace_parameters);
-
-  // Check key.
-  if (workspace_frame_id.empty())
-  {
-    return MoveItErrorCode(MoveItErrorCode::INVALID_MOTION_PLAN,
-                           name_ + ": Skipping insert: Workspace frame ID cannot be empty.");
-  }
-  if (key.goal_constraints.empty())
-  {
-    return MoveItErrorCode(MoveItErrorCode::INVALID_MOTION_PLAN, name_ + ": Skipping insert: No goal.");
-  }
-
-  // Check value.
-  if (value.trajectory.joint_trajectory.points.empty())
-  {
-    return MoveItErrorCode(MoveItErrorCode::INVALID_MOTION_PLAN, name_ + ": Empty joint trajectory points.");
-  }
-  if (value.trajectory.joint_trajectory.joint_names.empty())
-  {
-    return MoveItErrorCode(MoveItErrorCode::INVALID_MOTION_PLAN,
-                           name_ + ": Skipping insert: Empty joint trajectory joint names.");
-  }
-  if (!value.trajectory.multi_dof_joint_trajectory.points.empty())
-  {
-    return MoveItErrorCode(MoveItErrorCode::INVALID_MOTION_PLAN,
-                           name_ + ": Skipping insert: Multi-DOF trajectory plans are not supported.");
-  }
-  if (value.trajectory.joint_trajectory.header.frame_id.empty())
-  {
-    return MoveItErrorCode(MoveItErrorCode::INVALID_MOTION_PLAN,
-                           name_ + ": Skipping insert: Trajectory frame ID cannot be empty.");
-  }
-  if (workspace_frame_id != value.trajectory.joint_trajectory.header.frame_id)
-  {
-    std::stringstream ss;
-    ss << "Skipping insert: Plan request frame (" << workspace_frame_id << ") does not match plan frame ("
-       << value.trajectory.joint_trajectory.header.frame_id << ").";
-    return MoveItErrorCode(MoveItErrorCode::INVALID_MOTION_PLAN, ss.str());
-  }
-
-  return MoveItErrorCode::SUCCESS;
-}
-
-std::vector<MessageWithMetadata<RobotTrajectory>::ConstPtr> AlwaysInsertNeverPrunePolicy::fetchMatchingEntries(
-    const MoveGroupInterface& move_group, const MessageCollection<RobotTrajectory>& coll, const MotionPlanRequest& key,
-    const MoveGroupInterface::Plan& /*value*/, double exact_match_precision)
-{
-  Query::Ptr query = coll.createQuery();
-
-  // We avoid the heap allocation from getSupportedFeatures() for performance reasons.
-  // Furthermore, we set match_precision to zero because we want "exact" matches.
-
-  // Start.
-  if (MoveItErrorCode ret =
-          WorkspaceFeatures().appendFeaturesAsExactFetchQuery(*query, key, move_group, exact_match_precision);
-      !ret)
-  {
-    return {};
-  };
-  if (MoveItErrorCode ret = StartStateJointStateFeatures(/*match_precision=*/0.0)
-                                .appendFeaturesAsExactFetchQuery(*query, key, move_group, exact_match_precision);
-      !ret)
-  {
-    return {};
-  };
-
-  // Goal.
-  if (MoveItErrorCode ret = MaxSpeedAndAccelerationFeatures().appendFeaturesAsExactFetchQuery(*query, key, move_group,
-                                                                                              exact_match_precision);
-      !ret)
-  {
-    return {};
-  };
-  if (MoveItErrorCode ret = GoalConstraintsFeatures(/*match_precision=*/0.0)
-                                .appendFeaturesAsExactFetchQuery(*query, key, move_group, exact_match_precision);
-      !ret)
-  {
-    return {};
-  };
-  if (MoveItErrorCode ret = PathConstraintsFeatures(/*match_precision=*/0.0)
-                                .appendFeaturesAsExactFetchQuery(*query, key, move_group, exact_match_precision);
-      !ret)
-  {
-    return {};
-  };
-  if (MoveItErrorCode ret = TrajectoryConstraintsFeatures(/*match_precision=*/0.0)
-                                .appendFeaturesAsExactFetchQuery(*query, key, move_group, exact_match_precision);
-      !ret)
-  {
-    return {};
-  };
-
-  return coll.queryList(query, /*metadata_only=*/true);
-}
-
-bool AlwaysInsertNeverPrunePolicy::prunePredicate(
-    const MoveGroupInterface& /*move_group*/, const MotionPlanRequest& /*key*/,
-    const MoveGroupInterface::Plan& /*value*/, const MessageWithMetadata<RobotTrajectory>::ConstPtr& /*matched_entry*/)
-{
-  return false;  // Never prune.
-}
-
-bool AlwaysInsertNeverPrunePolicy::insertPredicate(const MoveGroupInterface& /*move_group*/,
-                                                   const MotionPlanRequest& /*key*/,
-                                                   const MoveGroupInterface::Plan& /*value*/)
-{
-  return true;  // Always insert.
-}
-
-MoveItErrorCode AlwaysInsertNeverPrunePolicy::appendInsertMetadata(Metadata& metadata,
-                                                                   const MoveGroupInterface& move_group,
-                                                                   const MotionPlanRequest& key,
-                                                                   const MoveGroupInterface::Plan& value)
-{
-  // Extract and append features.
-  // We avoid the heap allocation from getSupportedFeatures() for performance reasons.
-
-  // Start features.
-  if (MoveItErrorCode ret = WorkspaceFeatures().appendFeaturesAsInsertMetadata(metadata, key, move_group); !ret)
-  {
-    return ret;
-  }
-  if (MoveItErrorCode ret =
-          StartStateJointStateFeatures(/*match_tolerance=*/0).appendFeaturesAsInsertMetadata(metadata, key, move_group);
-      !ret)
-  {
-    return ret;
-  }
-
-  // Goal features.
-  if (MoveItErrorCode ret = MaxSpeedAndAccelerationFeatures().appendFeaturesAsInsertMetadata(metadata, key, move_group);
-      !ret)
-  {
-    return ret;
-  }
-  if (MoveItErrorCode ret =
-          GoalConstraintsFeatures(/*match_tolerance=*/0).appendFeaturesAsInsertMetadata(metadata, key, move_group);
-      !ret)
-  {
-    return ret;
-  }
-  if (MoveItErrorCode ret =
-          PathConstraintsFeatures(/*match_tolerance=*/0).appendFeaturesAsInsertMetadata(metadata, key, move_group);
-      !ret)
-  {
-    return ret;
-  }
-  if (MoveItErrorCode ret =
-          TrajectoryConstraintsFeatures(/*match_tolerance=*/0).appendFeaturesAsInsertMetadata(metadata, key, move_group);
-      !ret)
-  {
-    return ret;
-  }
-
-  // Append ValueT metadata.
-  metadata.append(EXECUTION_TIME,
-                  rclcpp::Duration(value.trajectory.joint_trajectory.points.back().time_from_start).seconds());
-  metadata.append(PLANNING_TIME, value.planning_time);
-
-  return MoveItErrorCode::SUCCESS;
-}
-
-void AlwaysInsertNeverPrunePolicy::reset()
-{
-  return;
-}
-
-// =================================================================================================
-// CartesianAlwaysInsertNeverPrunePolicy.
-// =================================================================================================
-// moveit_msgs::srv::GetCartesianPath::Request <=> moveit_msgs::srv::GetCartesianPath::Response
+// moveit_msgs::srv::GetCartesianPath::Request <=> moveit_msgs::srv::GetCartesianPath::Response ====
 
 CartesianAlwaysInsertNeverPrunePolicy::CartesianAlwaysInsertNeverPrunePolicy()
   : name_("CartesianAlwaysInsertNeverPrunePolicy")
@@ -404,7 +190,7 @@ std::vector<MessageWithMetadata<RobotTrajectory>::ConstPtr> CartesianAlwaysInser
     return {};
   }
 
-  return coll.queryList(query, /*metadata_only=*/true);
+  return coll.queryList(query);
 }
 
 bool CartesianAlwaysInsertNeverPrunePolicy::prunePredicate(
