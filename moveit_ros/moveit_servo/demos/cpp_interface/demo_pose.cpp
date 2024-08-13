2178ed0f21504a2504ddd7093e18d4bfef17b244
AndyZe <andyz@utexas.edu>
2024-08-13 04:48:33 -0500

    Ensure the robot state is up-to-date before Servoing (#2954)

--- moveit_ros/moveit_servo/demos/cpp_interface/demo_pose.cpp
+++ moveit_ros/moveit_servo/demos/cpp_interface/demo_pose.cpp
@@ -119,7 +119,7 @@ int main(int argc, char* argv[])
 
   // create command queue to build trajectory message and add current robot state
   std::deque<KinematicState> joint_cmd_rolling_window;
-  KinematicState current_state = servo.getCurrentRobotState();
+  KinematicState current_state = servo.getCurrentRobotState(true /* wait for updated state */);
   updateSlidingWindow(current_state, joint_cmd_rolling_window, servo_params.max_expected_latency, demo_node->now());
 
   bool satisfies_linear_tolerance = false;
