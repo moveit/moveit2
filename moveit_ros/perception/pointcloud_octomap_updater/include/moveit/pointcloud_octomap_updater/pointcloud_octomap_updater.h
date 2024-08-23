b006383e5188a5157510709b6db9e6c5177efc5f
Sebastian Jahr <sebastian.jahr@picknik.ai>
2024-08-23 12:00:52 +0200

    Use old header in older version

--- moveit_ros/perception/pointcloud_octomap_updater/include/moveit/pointcloud_octomap_updater/pointcloud_octomap_updater.h
+++ moveit_ros/perception/pointcloud_octomap_updater/include/moveit/pointcloud_octomap_updater/pointcloud_octomap_updater.h
@@ -39,7 +39,11 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/message_filter.h>
+#if RCLCPP_VERSION_GTE(28, 0, 0)  // Rolling
 #include <message_filters/subscriber.hpp>
+#else
+#include <message_filters/subscriber.h> // Older versions
+#endif
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <moveit/occupancy_map_monitor/occupancy_map_updater.h>
 #include <moveit/point_containment_filter/shape_mask.h>
