e32e6c60a2a56dcaca2d0915b80f81e2c75b6a82
Sebastian Jahr <sebastian.jahr@picknik.ai>
2024-08-23 12:35:39 +0200

    Revert + add correct header

--- moveit_ros/perception/pointcloud_octomap_updater/include/moveit/pointcloud_octomap_updater/pointcloud_octomap_updater.h
+++ moveit_ros/perception/pointcloud_octomap_updater/include/moveit/pointcloud_octomap_updater/pointcloud_octomap_updater.h
@@ -37,6 +37,7 @@
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/version.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/message_filter.h>
 #if RCLCPP_VERSION_GTE(28, 3, 0)  // Rolling
