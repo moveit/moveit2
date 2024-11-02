471374248f7a51d6aafc0913c63f7116655cf98b
Tom Noble <tom@rivelinrobotics.com>
2024-11-02 16:01:01 +0000

    Fixes compilation errors

--- moveit_planners/pilz_industrial_motion_planner/test/unit_tests/src/unittest_trajectory_functions.cpp
+++ moveit_planners/pilz_industrial_motion_planner/test/unit_tests/src/unittest_trajectory_functions.cpp
@@ -95,7 +95,7 @@ protected:
     rm_loader_ = std::make_unique<robot_model_loader::RobotModelLoader>(node_);
     robot_model_ = rm_loader_->getModel();
     ASSERT_TRUE(bool(robot_model_)) << "Failed to load robot model";
-    robot_state_ = robot_state::RobotState(robot_model_);
+    robot_state_ = moveit::core::RobotState(robot_model_);
     planning_scene_ = std::make_shared<planning_scene::PlanningScene>(robot_model_);
 
     // get parameters
@@ -139,7 +139,7 @@ protected:
   // ros stuff
   rclcpp::Node::SharedPtr node_;
   moveit::core::RobotModelConstPtr robot_model_;
-  robot_state::RobotState robot_state_;
+  moveit::core::RobotState robot_state_;
   std::unique_ptr<robot_model_loader::RobotModelLoader> rm_loader_;
   planning_scene::PlanningSceneConstPtr planning_scene_;
 
