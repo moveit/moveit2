a89a7d999b7ae74f23c38a651639481ba3baeef8
Andrea <realeandrea@yahoo.it>
2024-11-03 16:59:25 +0100

    fix clang

--- moveit_ros/robot_interaction/include/moveit/robot_interaction/interaction_handler.h
+++ moveit_ros/robot_interaction/include/moveit/robot_interaction/interaction_handler.h
@@ -39,7 +39,6 @@
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <moveit/macros/class_forward.h>
 #include <moveit/robot_interaction/locked_robot_state.h>
-//#include <moveit/robot_interaction/robot_interaction.h>
 #include <visualization_msgs/msg/interactive_marker_feedback.hpp>
 #include <interactive_markers/menu_handler.hpp>
 #include <tf2_ros/buffer.h>
