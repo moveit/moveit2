d2402a6a2347dc9986f17c4a85bca10276aed7da
Tom Noble <tom@rivelinrobotics.com>
2024-11-08 10:58:22 +0000

    Fixes RCLCPP_WARN calls

--- moveit_planners/pilz_industrial_motion_planner/src/trajectory_generator_ptp.cpp
+++ moveit_planners/pilz_industrial_motion_planner/src/trajectory_generator_ptp.cpp
@@ -239,8 +239,8 @@ void TrajectoryGeneratorPTP::extractMotionPlanInfo(const planning_scene::Plannin
     if (req.goal_constraints.front().position_constraints.front().header.frame_id.empty() ||
         req.goal_constraints.front().orientation_constraints.front().header.frame_id.empty())
     {
-      RCLCPP_WARN("Frame id is not set in position/orientation constraints of "
-                  "goal. Use model frame as default");
+      RCLCPP_WARN(getLogger(), "Frame id is not set in position/orientation constraints of "
+                               "goal. Use model frame as default");
       frame_id = robot_model_->getModelFrame();
     }
     else
