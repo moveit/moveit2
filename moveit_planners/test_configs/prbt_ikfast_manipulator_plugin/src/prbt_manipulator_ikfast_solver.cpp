79247692bff299aa363c66ecae0e8d4c27700e4f
Tom Noble <t.s.noble@outlook.com>
2024-11-17 21:24:28 +0000

    Fixes ikfast errors

--- moveit_planners/test_configs/prbt_ikfast_manipulator_plugin/src/prbt_manipulator_ikfast_solver.cpp
+++ moveit_planners/test_configs/prbt_ikfast_manipulator_plugin/src/prbt_manipulator_ikfast_solver.cpp
@@ -22,11 +22,11 @@
 /// To compile without any main function as a shared object (might need -llapack):
 ///     gcc -fPIC -lstdc++ -DIKFAST_NO_MAIN -DIKFAST_CLIBRARY -shared -Wl,-soname,libik.so -o libik.so ik.cpp
 #define IKFAST_HAS_LIBRARY
-#include "ikfast.hpp" // found inside share/openrave-X.Y/python/ikfast.hpp
+#include "ikfast.h" // found inside share/openrave-X.Y/python/ikfast.h
 using namespace ikfast;
 
 // check if the included ikfast version matches what this file was compiled with
-static_assert(IKFAST_VERSION==61); // version found in ikfast.hpp
+static_assert(IKFAST_VERSION==61); // version found in ikfast.h
 
 #include <cmath>
 #include <vector>
