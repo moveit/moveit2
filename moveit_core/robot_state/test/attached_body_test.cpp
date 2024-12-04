31f0cc62169964a769a31239173c78297736ae19
Tom Noble <t.s.noble@outlook.com>
2024-12-04 19:57:24 +0000

    Fixes fixtures

--- moveit_core/robot_state/test/attached_body_test.cpp
+++ moveit_core/robot_state/test/attached_body_test.cpp
@@ -46,7 +46,6 @@ class SingleLinkRobot : public ::testing::Test
 public:
   virtual void SetUp() override
   {
-    std::cout << "SingleLinkRobot" << std::endl;
     static const std::string URDF_XML = R"(
       <?xml version="1.0" ?>
       <robot name="single_link_robot">
@@ -69,21 +68,12 @@ public:
           </geometry>
         </visual>
       </link>
-      <joint name="joint" type="revolute">
-          <axis xyz="0 0 1"/>
-          <parent link="link"/>
-          <origin rpy="0 0 0" xyz="0 0 0"/>
-          <limit effort="100.0" lower="-3.14" upper="3.14" velocity="0.2"/>
-      </joint>
       </robot>
     )";
 
     static const std::string SRDF_XML = R"xml(
       <?xml version="1.0" ?>
       <robot name="single_link_robot">
-        <group name="joint_group">
-          <joint name="joint"/>
-        </group>
       </robot>
       )xml";
 
@@ -91,11 +81,6 @@ public:
     auto srdf_model = std::make_shared<srdf::Model>();
     srdf_model->initString(*urdf_model, SRDF_XML);
     robot_model_ = std::make_shared<moveit::core::RobotModel>(urdf_model, srdf_model);
-    std::cout << robot_model_->getName() << std::endl;
-  }
-
-  virtual void TearDown() override
-  {
   }
 
 protected:
@@ -104,10 +89,10 @@ protected:
 
 class SingleAttachedBody : public SingleLinkRobot
 {
-protected:
+public:
   virtual void SetUp() override
   {
-    std::cout << "SingleAttachedBody" << std::endl;
+    SingleLinkRobot::SetUp();  // Is this necessary?
     const moveit::core::LinkModel* link = robot_model_->getLinkModel("link");
     std::string name = "root_body";
     Eigen::Isometry3d pose;
@@ -129,10 +114,6 @@ protected:
     moveit::core::FixedTransformsMap subframes{ { "subframe", subframe_pose } };
     root_body_ = std::make_shared<moveit::core::AttachedBody>(link, name, pose, shapes, shape_poses, touch_links,
                                                               detach_posture, subframes);
-  }
-
-  virtual void TearDown() override
-  {
   }
 
 protected:
