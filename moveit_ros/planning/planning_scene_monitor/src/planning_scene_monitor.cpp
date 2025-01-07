dea3585a5a4aa23af7a3381d36fe5893c2afee6d
marqrazz <marq.razz@gmail.com>
2025-01-07 09:52:29 -0700

    Simplify scene update that does not include new robot_state

--- moveit_ros/planning/planning_scene_monitor/src/planning_scene_monitor.cpp
+++ moveit_ros/planning/planning_scene_monitor/src/planning_scene_monitor.cpp
@@ -740,21 +740,9 @@ bool PlanningSceneMonitor::newPlanningSceneMessage(const moveit_msgs::msg::Plann
 
     if (!scene.is_diff && parent_scene_)
     {
-      // if the scene does not contain a robot state then use the latest known values for the robot state
-      // so that we are not updating the scene with stale values.
-      if (scene.robot_state.joint_state.name.empty())
-      {
-        // if we have a valid current_state_monitor_ with complete state use the latest data otherwise try
-        // to use the maintained (diff) scene_ which may fall back to the parent_scene_ data and not update.
-        if (current_state_monitor_ && current_state_monitor_->haveCompleteState())
-        {
-          parent_scene_->setCurrentState(*current_state_monitor_->getCurrentState());
-        }
-        else
-        {
-          parent_scene_->setCurrentState(scene_->getCurrentState());
-        }
-      }
+      // If there is no new robot_state, transfer RobotState from current scene to parent scene
+      if (scene.robot_state.joint_state.name.empty() && scene.robot_state.multi_dof_joint_state.joint_names.empty())
+        parent_scene_->setCurrentState(scene_->getCurrentState());
       // clear maintained (diff) scene_ and set the full new scene in parent_scene_ instead
       scene_->clearDiffs();
       result = parent_scene_->setPlanningSceneMsg(scene);
