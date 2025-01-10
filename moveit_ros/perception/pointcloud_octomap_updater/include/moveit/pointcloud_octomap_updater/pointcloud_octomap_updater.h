dcdc12dea3a97ca2f6ed3f9e242a824fa2e77d4a
sea-bass <sebas.a.castro@gmail.com>
2025-01-09 21:27:09 -0500

    Add missing include

--- moveit_ros/perception/pointcloud_octomap_updater/include/moveit/pointcloud_octomap_updater/pointcloud_octomap_updater.h
+++ moveit_ros/perception/pointcloud_octomap_updater/include/moveit/pointcloud_octomap_updater/pointcloud_octomap_updater.h
@@ -38,6 +38,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/callback_group.hpp>
+#include <rclcpp/version.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/message_filter.h>
 #include <message_filters/subscriber.h>
