044b5ad5e05e9474b31b58126fd84d80f29f7bbc
Mark Johnson <104826595+rr-mark@users.noreply.github.com>
2025-02-06 02:27:04 +0000

    Ports moveit #3676 and #3682 (#3283)
    * Use separate callback queue + spinner for ExecuteTrajectoryAction (#3676)
    
    This allows parallel execution + planning.
    
    Also required modifying updateSceneWithCurrentState() to allow skipping a scene update with a new robot state (from CurrentStateMonitor), if the planning scene is currently locked (due to planning).
    Otherwise, the CurrentStateMonitor would block too.
    
    Co-authored-by: Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
    
    * PSM: simplify state_update_pending_ (#3682)
    
    * Move update of state_update_pending_ to updateSceneWithCurrentState()
    
    * Revert to try_lock
    
    While there are a few other locks except explicit user locks (getPlanningSceneServiceCallback(), collisionObjectCallback(), attachObjectCallback(), newPlanningSceneCallback(), and scenePublishingThread()), these occur rather seldom
    (scenePublishingThread() publishes at 2Hz).
    
    Hence, we might indeed balance a non-blocking CSM vs. missed PS updates in favour of CSM.
    
    * Don't block for scene update from stateUpdateTimerCallback too
    
    The timer callback and CSM's state update callbacks are served from the same callback queue, which would block CSM again.
    
    * further locking adaptations
    
    reading dt_state_update_ and last_robot_state_update_wall_time_
    does not lead to logic errors, but at most to a skipped or redundant update on corrupted data.
    Alternatively we could be on the safe side and turn both variables into std::atomic, but that
    would effectively mean locks on every read.
    
    Instead, only set state_update_pending_ as an atomic, which is lockfree in this case.
    
    Co-authored-by: Michael Görner <me@v4hn.de>
    
    * Ports changes to ROS2.
    
    ---------
    
    Co-authored-by: Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
    Co-authored-by: Robert Haschke <rhaschke@users.noreply.github.com>
    Co-authored-by: Michael Görner <me@v4hn.de>
    Co-authored-by: Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
    Co-authored-by: Sebastian Jahr <sebastian.jahr@picknik.ai>
    (cherry picked from commit ba35aaa58ca76762e531e932475cbfbbcd628702)
    
    # Conflicts:
    #	moveit_ros/move_group/src/default_capabilities/execute_trajectory_action_capability.cpp
    #	moveit_ros/planning/planning_scene_monitor/include/moveit/planning_scene_monitor/planning_scene_monitor.hpp
    #	moveit_ros/planning/planning_scene_monitor/src/planning_scene_monitor.cpp

--- moveit_ros/move_group/src/default_capabilities/execute_trajectory_action_capability.cpp
+++ moveit_ros/move_group/src/default_capabilities/execute_trajectory_action_capability.cpp
@@ -51,12 +51,24 @@ MoveGroupExecuteTrajectoryAction::MoveGroupExecuteTrajectoryAction() : MoveGroup
 {
 }
 
+MoveGroupExecuteTrajectoryAction::~MoveGroupExecuteTrajectoryAction()
+{
+  callback_executor_.cancel();
+
+  if (callback_thread_.joinable())
+    callback_thread_.join();
+}
+
 void MoveGroupExecuteTrajectoryAction::initialize()
 {
   using std::placeholders::_1;
   using std::placeholders::_2;
 
   auto node = context_->moveit_cpp_->getNode();
+  callback_group_ = node->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive,
+                                                false /* don't spin with node executor */);
+  callback_executor_.add_callback_group(callback_group_, node->get_node_base_interface());
+  callback_thread_ = std::thread([this]() { callback_executor_.spin(); });
   // start the move action server
   execute_action_server_ = rclcpp_action::create_server<ExecTrajectory>(
       node->get_node_base_interface(), node->get_node_clock_interface(), node->get_node_logging_interface(),
@@ -65,11 +77,17 @@ void MoveGroupExecuteTrajectoryAction::initialize()
         RCLCPP_INFO(LOGGER, "Received goal request");
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
       },
+<<<<<<< HEAD
       [](const std::shared_ptr<ExecTrajectoryGoal>& /* unused */) {
         RCLCPP_INFO(LOGGER, "Received request to cancel goal");
         return rclcpp_action::CancelResponse::ACCEPT;
       },
       [this](const auto& goal) { executePathCallback(goal); });
+=======
+      [](const std::shared_ptr<ExecTrajectoryGoal>& /* unused */) { return rclcpp_action::CancelResponse::ACCEPT; },
+      [this](const auto& goal) { executePathCallback(goal); }, rcl_action_server_get_default_options(),
+      callback_group_);
+>>>>>>> ba35aaa58 (Ports moveit #3676 and #3682 (#3283))
 }
 
 void MoveGroupExecuteTrajectoryAction::executePathCallback(const std::shared_ptr<ExecTrajectoryGoal>& goal)
