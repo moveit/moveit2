6df5901e3e34667eb94cf04f85d44b338d94c5f5
Vibhu_Sharma <114796928+VibhuSharma19@users.noreply.github.com>
2025-04-08 15:12:19 +0530

    Moved class description docstring to the correct location

--- moveit_py/moveit/core/__init__.pyi
+++ moveit_py/moveit/core/__init__.pyi
@@ -1,6 +1,6 @@
 class World:
-    def __init__(self) -> None:
-        """
+    """
         Maintain a representation of the environment
-        """
-        ...
+    """
+
+    def __init__(self) -> None:  ...
