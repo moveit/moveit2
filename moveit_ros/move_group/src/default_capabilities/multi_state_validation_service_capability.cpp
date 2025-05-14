7833ce2f4aceb357995c3d156f069b990265c12e
Lucian James <ljames@rivelinrobotics.com>
2025-05-14 11:05:20 +0100

    format with the right format

--- moveit_ros/move_group/src/default_capabilities/multi_state_validation_service_capability.cpp
+++ moveit_ros/move_group/src/default_capabilities/multi_state_validation_service_capability.cpp
@@ -5,54 +5,49 @@
 #include <moveit/robot_state/conversions.hpp>
 #include <moveit/utils/message_checks.hpp>
 
-namespace move_group {
+namespace move_group
+{
 
-void MoveGroupMultiStateValidationService::initialize() {
-  validity_service_ =
-      context_->moveit_cpp_->getNode()
-          ->create_service<moveit_msgs::srv::GetMultiStateValidity>(
-              MULTI_STATE_VALIDITY_SERVICE_NAME,
-              [this](
-                  const std::shared_ptr<rmw_request_id_t> &request_header,
-                  const std::shared_ptr<
-                      moveit_msgs::srv::GetMultiStateValidity::Request> &req,
-                  const std::shared_ptr<
-                      moveit_msgs::srv::GetMultiStateValidity::Response> &res) {
-                return computeService(request_header, req, res);
-              });
+void MoveGroupMultiStateValidationService::initialize()
+{
+  validity_service_ = context_->moveit_cpp_->getNode()->create_service<moveit_msgs::srv::GetMultiStateValidity>(
+      MULTI_STATE_VALIDITY_SERVICE_NAME,
+      [this](const std::shared_ptr<rmw_request_id_t>& request_header,
+             const std::shared_ptr<moveit_msgs::srv::GetMultiStateValidity::Request>& req,
+             const std::shared_ptr<moveit_msgs::srv::GetMultiStateValidity::Response>& res) {
+        return computeService(request_header, req, res);
+      });
 }
 
 bool MoveGroupMultiStateValidationService::computeService(
-    const std::shared_ptr<rmw_request_id_t> & /* unused */,
-    const std::shared_ptr<moveit_msgs::srv::GetMultiStateValidity::Request>
-        &req,
-    const std::shared_ptr<moveit_msgs::srv::GetMultiStateValidity::Response>
-        &res) {
-  planning_scene_monitor::LockedPlanningSceneRO ls(
-      context_->planning_scene_monitor_);
+    const std::shared_ptr<rmw_request_id_t>& /* unused */,
+    const std::shared_ptr<moveit_msgs::srv::GetMultiStateValidity::Request>& req,
+    const std::shared_ptr<moveit_msgs::srv::GetMultiStateValidity::Response>& res)
+{
+  planning_scene_monitor::LockedPlanningSceneRO ls(context_->planning_scene_monitor_);
   moveit::core::RobotState rs = ls->getCurrentState();
   moveit::core::robotStateMsgToRobotState(req->robot_state, rs);
-  for (size_t i = 0; i < req->joint_states.size(); ++i) {
+  for (size_t i = 0; i < req->joint_states.size(); ++i)
+  {
     // Update robot state with next set of joint states
     rs.setVariableValues(req->joint_states[i]);
 
     // Check validity of given joint state
-    res->valid =
-        isStateValid(ls, rs, req->group_name, req->constraints, res->contacts,
-                     res->cost_sources, res->constraint_result);
+    res->valid = isStateValid(ls, rs, req->group_name, req->constraints, res->contacts, res->cost_sources,
+                              res->constraint_result);
 
     // This service only checks up to the first invalid joint state,
     // with the result then holding the information about the first invalid
     // state
-    if (!res->valid) {
+    if (!res->valid)
+    {
       break;
     }
   }
   return true;
 }
-} // namespace move_group
+}  // namespace move_group
 
 #include <pluginlib/class_list_macros.hpp>
 
-PLUGINLIB_EXPORT_CLASS(move_group::MoveGroupMultiStateValidationService,
-                       move_group::MoveGroupCapability)
+PLUGINLIB_EXPORT_CLASS(move_group::MoveGroupMultiStateValidationService, move_group::MoveGroupCapability)
--- moveit_ros/move_group/src/default_capabilities/state_validation_service_capability.cpp
+++ moveit_ros/move_group/src/default_capabilities/state_validation_service_capability.cpp
@@ -41,31 +41,29 @@
 #include <moveit/robot_state/conversions.hpp>
 #include <moveit/utils/message_checks.hpp>
 
-namespace move_group {
-MoveGroupStateValidationService::MoveGroupStateValidationService()
-    : MoveGroupCapability("state_validation_service") {}
+namespace move_group
+{
+MoveGroupStateValidationService::MoveGroupStateValidationService() : MoveGroupCapability("state_validation_service")
+{
+}
 
-void MoveGroupStateValidationService::initialize() {
-  validity_service_ =
-      context_->moveit_cpp_->getNode()
-          ->create_service<moveit_msgs::srv::GetStateValidity>(
-              STATE_VALIDITY_SERVICE_NAME,
-              [this](const std::shared_ptr<rmw_request_id_t> &request_header,
-                     const std::shared_ptr<
-                         moveit_msgs::srv::GetStateValidity::Request> &req,
-                     const std::shared_ptr<
-                         moveit_msgs::srv::GetStateValidity::Response> &res) {
-                return computeService(request_header, req, res);
-              });
+void MoveGroupStateValidationService::initialize()
+{
+  validity_service_ = context_->moveit_cpp_->getNode()->create_service<moveit_msgs::srv::GetStateValidity>(
+      STATE_VALIDITY_SERVICE_NAME, [this](const std::shared_ptr<rmw_request_id_t>& request_header,
+                                          const std::shared_ptr<moveit_msgs::srv::GetStateValidity::Request>& req,
+                                          const std::shared_ptr<moveit_msgs::srv::GetStateValidity::Response>& res) {
+        return computeService(request_header, req, res);
+      });
 }
 
 bool MoveGroupStateValidationService::isStateValid(
-    const planning_scene_monitor::LockedPlanningSceneRO &ls,
-    const moveit::core::RobotState &rs, const std::string &group_name,
-    const moveit_msgs::msg::Constraints &constraints,
-    std::vector<moveit_msgs::msg::ContactInformation> &contacts,
-    std::vector<moveit_msgs::msg::CostSource> &cost_sources,
-    std::vector<moveit_msgs::msg::ConstraintEvalResult> &constraint_result) {
+    const planning_scene_monitor::LockedPlanningSceneRO& ls, const moveit::core::RobotState& rs,
+    const std::string& group_name, const moveit_msgs::msg::Constraints& constraints,
+    std::vector<moveit_msgs::msg::ContactInformation>& contacts,
+    std::vector<moveit_msgs::msg::CostSource>& cost_sources,
+    std::vector<moveit_msgs::msg::ConstraintEvalResult>& constraint_result)
+{
   bool valid = true;
 
   // configure collision request
@@ -73,9 +71,7 @@ bool MoveGroupStateValidationService::isStateValid(
   creq.group_name = group_name;
   creq.cost = true;
   creq.contacts = true;
-  creq.max_contacts =
-      ls->getWorld()->size() +
-      ls->getRobotModel()->getLinkModelsWithCollisionGeometry().size();
+  creq.max_contacts = ls->getWorld()->size() + ls->getRobotModel()->getLinkModelsWithCollisionGeometry().size();
   creq.max_cost_sources = creq.max_contacts;
   creq.max_contacts *= creq.max_contacts;
   collision_detection::CollisionResult cres;
@@ -84,15 +80,15 @@ bool MoveGroupStateValidationService::isStateValid(
   ls->checkCollision(creq, cres, rs);
 
   // copy contacts if any
-  if (cres.collision) {
-    rclcpp::Time time_now =
-        context_->moveit_cpp_->getNode()->get_clock()->now();
+  if (cres.collision)
+  {
+    rclcpp::Time time_now = context_->moveit_cpp_->getNode()->get_clock()->now();
     contacts.reserve(cres.contact_count);
     valid = false;
-    for (collision_detection::CollisionResult::ContactMap::const_iterator it =
-             cres.contacts.begin();
+    for (collision_detection::CollisionResult::ContactMap::const_iterator it = cres.contacts.begin();
          it != cres.contacts.end(); ++it)
-      for (const collision_detection::Contact &contact : it->second) {
+      for (const collision_detection::Contact& contact : it->second)
+      {
         contacts.resize(contacts.size() + 1);
         collision_detection::contactToMsg(contact, contacts.back());
         contacts.back().header.frame_id = ls->getPlanningFrame();
@@ -102,25 +98,28 @@ bool MoveGroupStateValidationService::isStateValid(
 
   // copy cost sources
   cost_sources.reserve(cres.cost_sources.size());
-  for (const collision_detection::CostSource &cost_source : cres.cost_sources) {
+  for (const collision_detection::CostSource& cost_source : cres.cost_sources)
+  {
     cost_sources.resize(cost_sources.size() + 1);
     collision_detection::costSourceToMsg(cost_source, cost_sources.back());
   }
 
   // evaluate constraints
-  if (!moveit::core::isEmpty(constraints)) {
+  if (!moveit::core::isEmpty(constraints))
+  {
     kinematic_constraints::KinematicConstraintSet kset(ls->getRobotModel());
     kset.add(constraints, ls->getTransforms());
     std::vector<kinematic_constraints::ConstraintEvaluationResult> kres;
-    kinematic_constraints::ConstraintEvaluationResult total_result =
-        kset.decide(rs, kres);
-    if (!total_result.satisfied) {
+    kinematic_constraints::ConstraintEvaluationResult total_result = kset.decide(rs, kres);
+    if (!total_result.satisfied)
+    {
       valid = false;
     }
 
     // copy constraint results
     constraint_result.resize(kres.size());
-    for (std::size_t k = 0; k < kres.size(); ++k) {
+    for (std::size_t k = 0; k < kres.size(); ++k)
+    {
       constraint_result[k].result = kres[k].satisfied;
       constraint_result[k].distance = kres[k].distance;
     }
@@ -130,21 +129,19 @@ bool MoveGroupStateValidationService::isStateValid(
 }
 
 bool MoveGroupStateValidationService::computeService(
-    const std::shared_ptr<rmw_request_id_t> & /* unused */,
-    const std::shared_ptr<moveit_msgs::srv::GetStateValidity::Request> &req,
-    const std::shared_ptr<moveit_msgs::srv::GetStateValidity::Response> &res) {
-  planning_scene_monitor::LockedPlanningSceneRO ls(
-      context_->planning_scene_monitor_);
+    const std::shared_ptr<rmw_request_id_t>& /* unused */,
+    const std::shared_ptr<moveit_msgs::srv::GetStateValidity::Request>& req,
+    const std::shared_ptr<moveit_msgs::srv::GetStateValidity::Response>& res)
+{
+  planning_scene_monitor::LockedPlanningSceneRO ls(context_->planning_scene_monitor_);
   moveit::core::RobotState rs = ls->getCurrentState();
   moveit::core::robotStateMsgToRobotState(req->robot_state, rs);
   res->valid =
-      isStateValid(ls, rs, req->group_name, req->constraints, res->contacts,
-                   res->cost_sources, res->constraint_result);
+      isStateValid(ls, rs, req->group_name, req->constraints, res->contacts, res->cost_sources, res->constraint_result);
   return true;
 }
-} // namespace move_group
+}  // namespace move_group
 
 #include <pluginlib/class_list_macros.hpp>
 
-PLUGINLIB_EXPORT_CLASS(move_group::MoveGroupStateValidationService,
-                       move_group::MoveGroupCapability)
+PLUGINLIB_EXPORT_CLASS(move_group::MoveGroupStateValidationService, move_group::MoveGroupCapability)
