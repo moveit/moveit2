7cf909635184842fcd5f1f8182810035771e5902
thomaspeyrucain <thomas.peyrucain@pal-robotics.com>
2025-08-14 17:32:35 +0200

    Fix ERROR stream + check only active controllers

--- moveit_plugins/moveit_ros_control_interface/src/controller_manager_plugin.cpp
+++ moveit_plugins/moveit_ros_control_interface/src/controller_manager_plugin.cpp
@@ -567,10 +567,10 @@ public:
     dependency_map_reverse_.clear();
     for (auto& controller : result->controller)
     {
-      if (controller.chain_connections.size() > 1)
+      if (isActive(controller) && controller.chain_connections.size() > 1)
       {
-        RCLCPP_ERROR_STREAM(LOGGER, "Controller with name %s chains to more than one controller. Chaining to more than "
-                                    "one controller is not supported.");
+        RCLCPP_ERROR_STREAM(LOGGER, "Controller with name " <<  controller.name << " chains to more than one controller. "
+                                    "Chaining to more than one controller is not supported.");
         return false;
       }
       for (const auto& chained_controller : controller.chain_connections)
