ad5384906f2154730b2376c2574a5084b8314596
Michael Carlstrom <rmc@carlstrom.com>
2025-12-14 13:55:53 -0800

    remove unused boost-filesystem
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- moveit_ros/planning/ConfigExtras.cmake
+++ moveit_ros/planning/ConfigExtras.cmake
@@ -1,4 +1,4 @@
 # Extras module needed for dependencies to find boost components
 
-find_package(Boost CONFIG REQUIRED COMPONENTS filesystem date_time
-                                              program_options thread chrono)
+find_package(Boost CONFIG REQUIRED COMPONENTS date_time program_options thread
+                                              chrono)
