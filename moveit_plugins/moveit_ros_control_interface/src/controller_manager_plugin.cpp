9deba66bec876d3c3a4e6f8c154fc698ad97dc11
thomas.peyrucain <thomas.peyrucain@pal-robotics.com>
2026-01-23 10:56:33 +0100

    pre-commit fix

--- moveit_plugins/moveit_ros_control_interface/src/controller_manager_plugin.cpp
+++ moveit_plugins/moveit_ros_control_interface/src/controller_manager_plugin.cpp
@@ -598,9 +598,10 @@ public:
     {
       if (isActive(controller) && controller.chain_connections.size() > 1)
       {
-        RCLCPP_ERROR_STREAM(getLogger(),
-                            "Controller with name " <<  controller.name << " chains to more than one controller. "
-                            "Chaining to more than one controller is not supported.");
+        RCLCPP_ERROR_STREAM(getLogger(), "Controller with name "
+                                             << controller.name
+                                             << " chains to more than one controller. "
+                                                "Chaining to more than one controller is not supported.");
         return false;
       }
       for (const auto& chained_controller : controller.chain_connections)
