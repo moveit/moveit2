7dd1e652da5c5e52fd259b16c3dec9c77f632d7c
Tom Noble <tnoble@rivelinrobotics.com>
2026-03-19 12:16:40 +0000

    Applies formatting

--- moveit_planners/pilz_industrial_motion_planner/src/trajectory_generator_lin.cpp
+++ moveit_planners/pilz_industrial_motion_planner/src/trajectory_generator_lin.cpp
@@ -67,8 +67,7 @@ rclcpp::Logger getLogger()
 }  // namespace
 TrajectoryGeneratorLIN::TrajectoryGeneratorLIN(const moveit::core::RobotModelConstPtr& robot_model,
                                                const LimitsContainer& planner_limits,
-                                               const pilz_sampling::Params& sampling,
-                                               const std::string& /*group_name*/)
+                                               const pilz_sampling::Params& sampling, const std::string& /*group_name*/)
   : TrajectoryGenerator::TrajectoryGenerator(robot_model, planner_limits, sampling)
 {
   planner_limits_.printCartesianLimits();
--- moveit_planners/pilz_industrial_motion_planner/src/trajectory_generator_ptp.cpp
+++ moveit_planners/pilz_industrial_motion_planner/src/trajectory_generator_ptp.cpp
@@ -56,8 +56,7 @@ rclcpp::Logger getLogger()
 }  // namespace
 TrajectoryGeneratorPTP::TrajectoryGeneratorPTP(const moveit::core::RobotModelConstPtr& robot_model,
                                                const LimitsContainer& planner_limits,
-                                               const pilz_sampling::Params& sampling,
-                                               const std::string& group_name)
+                                               const pilz_sampling::Params& sampling, const std::string& group_name)
   : TrajectoryGenerator::TrajectoryGenerator(robot_model, planner_limits, sampling)
 {
   if (!planner_limits_.hasJointLimits())
