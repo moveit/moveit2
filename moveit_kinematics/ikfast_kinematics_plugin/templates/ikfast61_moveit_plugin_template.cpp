a5399c713efd01d2e8f32407cd5203979e31dce1
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2026-08-14 19:52:12 +0200

    Guard clang-specific pragma

--- moveit_kinematics/ikfast_kinematics_plugin/templates/ikfast61_moveit_plugin_template.cpp
+++ moveit_kinematics/ikfast_kinematics_plugin/templates/ikfast61_moveit_plugin_template.cpp
@@ -808,10 +808,14 @@ bool IKFastKinematicsPlugin::getPositionFK(const std::vector<std::string>& link_
     return false;
   }
 
+#ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wvla-cxx-extension"
+#endif
   IkReal angles[num_joints_];
+#ifdef __clang__
 #pragma clang diagnostic pop
+#endif
   for (unsigned char i = 0; i < num_joints_; ++i)
     angles[i] = joint_angles[i];
 
